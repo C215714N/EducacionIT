2c458eb38f525f97e79f2b3b31d0c2281485244d
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-05-26 22:08:59 -0300

    Formularios con Estilos CSS

--- contacto.php
+++ contacto.php
@@ -1,23 +1,24 @@
 <?php
 // captura de variables
 //valor que se almacena en la memoria y que puede cambiar
-$nombre 	= 	$_POST['nombre'];
-$email 		= 	$_POST['email'];
-$tel 		= 	$_POST['tel'];
-$consulta 	= 	$_POST['consulta'];
+
+//PHP 			//HTML
+$nombre 	= 	$_POST['nombre']; 	//Cristian
+$email 		= 	$_POST['email']; 	//CristianDracedo@hotmail.com
+$consulta 	= 	$_POST['consulta']; //Buenas Noches, America!!
 
 // cuerpo del mail
 //Estructura del mensaje que se enviara
 $mensaje="
-<strong>Nombre:</strong> $nombre<br/>
-<strong>email:</strong> $email<br/>
-<strong>tel:</strong> $tel <br/> 
+<strong>Nombre:</strong> $nombre<br/>		
+<strong>email:</strong> $email<br/> 
 <strong>consulta:</strong> $consulta<br/>
 ";
 
 // envío del mail
 //Informacion utilizada por el servidor para generar el mensaje
-mail("micorreo@midominio.com", "Consulta Web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8");
+	//destinatario			//asunto		//cuerpo	//remitente
+mail("micorreo@midominio.com","Consulta Web",$mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8");
 
 //Redireccionamiento a pagina Web
 //Actualiza la pagina a la URL especificada
