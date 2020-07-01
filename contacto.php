faf8dc76efbf1d44dea03af95c29c53f280eda2d
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-06-30 22:13:50 -0300

    Formulario CSS

--- contacto.php
+++ contacto.php
@@ -1,21 +1,25 @@
 <?php
-// captura de variables
-$nombre=$_POST['nombre'];
-$email=$_POST['email'];
-$tel=$_POST['tel'];
-$consulta=$_POST['consulta'];
+	// VARIABLE: Espacio reservado en la memoria cuyo valor puede cambiar
+	// captura de variables.
+	//	PHP 			HTML
+		$nombre 	=	$_POST['nombre']; 	//Cristian
+		$email 		=	$_POST['email'];	//cristiandracedo@hotmail.com
+		$tel 		=	$_POST['tel'];		//
+		$nac 		= 	$_POST['nac'];
+		$consulta 	=	$_POST['consulta'];	//¡¡Buenas noches, America!!
 
-// cuerpo del mail
-$mensaje="
-<strong>Nombre:</strong> $nombre<br/>
-<strong>email:</strong> $email<br/>
-<strong>tel:</strong> $tel <br/> 
-<strong>consulta:</strong> $consulta<br/>
-";
+	// cuerpo del mail
+	$mensaje="
+	<strong>Nombre:</strong> $nombre<br/>
+	<strong>email:</strong> $email<br/>
+	<strong>tel:</strong> $tel <br/> 
+	<strong>consulta:</strong> $consulta<br/>
+	";
 
-// envío del mail
-mail("micorreo@midominio.com", "Consulta Web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8\n");
+	// envío del mail
+		//destinatario		//asunto		//cuerpo  //remitente
+	mail("info@misitio.com", "presupuesto", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8");
 
-//Redireccionamiento a pagina Web
-header ("Location: http://www.misitio.com/mipagina.html");
+	//Redireccionamiento a pagina Web
+	header("Location: http://www.misitio.com/contacto.html");
 ?>
