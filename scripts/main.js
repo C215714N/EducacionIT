3ac4e4a3b2733ba307384c82176eb5e0926edc22
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-07-22 18:22:12 -0300

    API's de Dibujo

--- scripts/main.js
+++ scripts/main.js
@@ -22,9 +22,12 @@
 		let ctx = document.querySelector('#jsCanvas').getContext('2d');
 		//Rectangulo Rojo
 			ctx.fillStyle = 'tomato';
-			ctx.fillRect(10, 10, 50, 50);
+			ctx.fillRect(10, 10, 50, 50); //(x, y, width, height)
 		//Circulo
 			ctx.fillStyle = 'rgba(30, 80, 150, .5)';
-			ctx.arc(60,60,25,25,100);
-			ctx.fill()
+			ctx.arc(60,60,25,25,100);//(x, y, rWidth, rHeight, arc)
+			ctx.fill();
+		//Texto
+			ctx.font = '16px arial';
+			ctx.strokeText('Canvas', 10, 100); //(Text, x, y)
 //Autor: Cristian Racedo
