94fd687f6a5b6ec8ef5ce77ebe79f415040a93cd
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-08-04 18:17:32 -0300

    tablas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+/*Menu desplegable*/
 document.querySelector('#menu').addEventListener(
 	'click', () => document.querySelector('.menu').classList.toggle('activado')
+)
+/*Abrir Formulario con Boton Flotante*/
+document.querySelector('#contactButton').addEventListener(
+	'click', () => document.querySelector('.modal').classList.add('modal-open')
+)
+/*Cerrar Formulario con Boton Absoluta*/
+document.querySelector('#cerrarForm').addEventListener(
+	'click', () => document.querySelector('.modal').classList.remove('modal-open')
 )
