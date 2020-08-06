789051cbdaa7683cdccba99d6a4d3069636d2beb
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-08-06 19:46:33 -0300

    MySQL Workbench

---
+++ educationit-ddl.sql
@@ -0,0 +1,20 @@
+-- Creacion y uso de la Base de datos
+CREATE DATABASE educacionit;
+USE educacionit;
+
+DROP TABLE alumnos IF EXISTS;
+
+CREATE TABLE alumnos(
+	id_alumno INT AUTO_INCREMENT,
+	apellido VARCHAR(100),
+	nombre VARCHAR(100),
+	tipo_doc ENUM('dni','ci','le','lc','pass'),
+	num_doc INT,
+	direccion VARCHAR(200),
+	telefono INT,
+	correo VARCHAR(200),
+	PRIMARY KEY (id_alumno),
+	UNIQUE KEY (tipo_doc, num_doc),
+);
+ALTER TABLE alumnos 
+	ADD estado BOOLEAN;
