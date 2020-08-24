4a1b6e01af5c51008a013e512488fc591a3a7f97
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-08-24 13:13:32 -0300

    Formularios con CSS

--- contacto.php
+++ contacto.php
@@ -3,19 +3,18 @@
 	//	PHP 			HTML
 		$nombre 	=	$_POST['nombre']; 	//Cristian
 		$email 		=	$_POST['email'];	//cristiandracedo@hotmail.com
-		$consulta 	=	$_POST['consulta'];	//¡¡Buenas Tardes, America!!
-
+		$consulta 	=	$_POST['consulta'];	//¡¡Buenos Dias, America!!
+		$telefono	=	$_POST['telefono']; //0303-456
 	// cuerpo del mail
 		$mensaje	= "
 			<strong>Nombre:</strong> $nombre <br/>
+			<strong>Telefono: </strong> $telefono </br>
 			<strong>email:</strong> $email <br/>
 			<strong>consulta:</strong> $consulta <br/>
 		";
-
 	// envío del mail
-		//destinatario		//asunto		//cuerpo  	//remitente
-	mail("info@misitio.com", "consulta web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8");
-
+			//destinatario		//asunto		//cuerpo  	//remitente
+		mail("info@misitio.com", "consulta web", $mensaje, "From: $nombre <$email> \n Reply-To:$email \n Content-Type: text/html; charset=utf-8");
 	//Redireccionamiento a pagina Web
-	header("Location: http://www.misitio.com/contacto.html");
+		header("Location: http://www.misitio.com/contacto.html");
 ?>
