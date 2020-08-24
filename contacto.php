318f43a2cd9cae3a7a51be86ac34faa1484dd8c5
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-08-24 09:23:03 -0300

    Script de Contacto

---
+++ contacto.php
@@ -0,0 +1,21 @@
+<?php
+	// VARIABLE: Es un espacio reservado en la memoria (TEMPORALMENTE) para almacenar datos, cuyo valor puede cambiar.
+	//	PHP 			HTML
+		$nombre 	=	$_POST['nombre']; 	//Cristian
+		$email 		=	$_POST['email'];	//cristiandracedo@hotmail.com
+		$consulta 	=	$_POST['consulta'];	//¡¡Buenas Tardes, America!!
+
+	// cuerpo del mail
+		$mensaje	= "
+			<strong>Nombre:</strong> $nombre <br/>
+			<strong>email:</strong> $email <br/>
+			<strong>consulta:</strong> $consulta <br/>
+		";
+
+	// envío del mail
+		//destinatario		//asunto		//cuerpo  	//remitente
+	mail("info@misitio.com", "consulta web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8");
+
+	//Redireccionamiento a pagina Web
+	header("Location: http://www.misitio.com/contacto.html");
+?>
