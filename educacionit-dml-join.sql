6ff7f8ee2a3f3b26b1bb72b68b29d60935cbee7f
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-08-27 20:36:20 -0300

    Consulta 4 tablas

--- educacionit-dml-join.sql
+++ educacionit-dml-join.sql
@@ -13,3 +13,27 @@
 	INNER JOIN cursos AS c
 	ON clases.id_curso = c.id_curso
 	LIMIT 20;
+    
+	-- Clases_Detalle
+    SELECT 
+		cd.id_detalle,
+        a.apellido AS "Alumno - Apellido",
+        a.nombre AS "Alumno - Nombre",
+        p.apellido AS "Profesor - Apellido",
+        p.nombre AS "Profesor - Nombre",
+        cur.nombre AS "Curso",
+        cla.dia,
+        cla.hora,
+        cd.observaciones,
+        count(cd.id_detalle) AS total
+    FROM clases_detalle AS cd
+    JOIN alumnos AS a
+		ON cd.id_alumno = a.id_alumno
+    JOIN clases AS cla
+		ON cd.id_clase = cla.id_clase
+    JOIN profesores AS p
+		ON cla.id_profesor = p.id_profesor
+    JOIN cursos AS cur
+		ON cla.id_curso = cur.id_curso
+    GROUP BY curso, dia, hora
+    HAVING total >= 10;
