c9fe94c79495f5f503b0bcff40347d02cd1fbbfd
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-09-08 20:30:58 -0300

    Manipulacion Video

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
 /*Maquetado Avanzado*/
+	//Reproduccion de contenido Multimedia
+	let play = true;
+	document.querySelector('#videoButton').addEventListener(
+		'click' , () => {
+			if (play) {
+				document.querySelector('#videoTag').play();
+				document.querySelector('#videoButton').innerHTML = "PAUSE";
+				play = false;
+			} else {
+				document.querySelector('#videoTag').pause();
+				document.querySelector('#videoButton').innerHTML = "PLAY";
+				play = true;
+			}
+		}
+	)
 
 /*Autor: Cristian Racedo*/
