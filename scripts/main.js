3b4421afe5dcbab5ee3703bb098a1f330deadc09
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-09-08 22:11:43 -0300

    Menu desplegable

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
 /*Maquetado Avanzado*/
+	//Menu Desplegable
+		let openMenu = true;
+		document.querySelector('#menu').addEventListener(
+			'click', () => {
+				if (openMenu) {
+					document.querySelector('.menu').classList.add('openMenu');
+					document.querySelector('#menu').innerHTML = '&times;';
+					openMenu = false;
+				} else{
+					document.querySelector('.menu').classList.remove('openMenu');
+					document.querySelector('#menu').innerHTML = '&equiv;';
+					openMenu = true;
+				}
+			}
+		)
 	//Reproduccion de contenido Multimedia
 		let play = true;
 		document.querySelector('#videoButton').addEventListener(
