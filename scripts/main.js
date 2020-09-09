a7d84fafd021fd7c94575ea6ce5ad65cfc8ed45f
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-09-08 21:33:49 -0300

    Elementos Estadisticos

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
 /*Maquetado Avanzado*/
 	//Reproduccion de contenido Multimedia
-	let play = true;
-	document.querySelector('#videoButton').addEventListener(
-		'click' , () => {
-			if (play) {
-				document.querySelector('#videoTag').play();
-				document.querySelector('#videoButton').innerHTML = "PAUSE";
-				play = false;
-			} else {
-				document.querySelector('#videoTag').pause();
-				document.querySelector('#videoButton').innerHTML = "PLAY";
-				play = true;
+		let play = true;
+		document.querySelector('#videoButton').addEventListener(
+			'click' , () => {
+				if (play) {
+					document.querySelector('#videoTag').play();
+					document.querySelector('#videoButton').innerHTML = "PAUSE";
+					play = false;
+				} else {
+					document.querySelector('#videoTag').pause();
+					document.querySelector('#videoButton').innerHTML = "PLAY";
+					play = true;
+				}
 			}
-		}
-	)
-
+		)
+	//Manipulacion Barra de Progreso
+		document.querySelector('#progressInput').addEventListener(
+			'change', () => document.querySelector('#progressBar').value = document.querySelector('#progressInput').value
+		)
 /*Autor: Cristian Racedo*/
