fde50a66ce42a0bc39ff1ec05e0c71b33b2bc813
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-09-16 22:04:36 -0300

    Creacion de TABLAS

--- DDL-tienda_online.sql
+++ DDL-tienda_online.sql
@@ -31,4 +31,26 @@
         ENUM		LISTA OPCIONES	('mañana','tarde','noche')
         
 */
-
+	CREATE TABLE clientes (
+		id_cliente INT AUTO_INCREMENT,
+        apellido VARCHAR(50) NOT NULL,
+        nombre VARCHAR(50) NOT NULL,
+        tipo_doc ENUM(
+			'dni',
+            'ci',
+            'lc',
+            'le',
+            'pass') NOT NULL,
+		num_doc INT UNSIGNED NOT NULL,
+        fecha_nac DATE,
+        direccion VARCHAR(200),
+        cp INT UNSIGNED,
+        telefono VARCHAR(20) NOT NULL,
+        correo VARCHAR(200),
+        observaciones VARCHAR(250),
+        PRIMARY KEY (id_cliente), -- Clave Principal
+        UNIQUE KEY (tipo_doc, num_doc) -- Clave unica (No se puede repetir tipo y numero a la vez)
+    );
+    
+SHOW DATABASES; -- muestra los esquemas alojados en el servidor
+SHOW TABLES; -- muestra las tablas de la base de datos actual
