805c0e69069d7019c381d29991111d4916b7c69f
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-09-28 22:11:52 -0300

    Formularios CSS

--- contacto.php
+++ contacto.php
@@ -1,3 +1,25 @@
 <?php 
-	echo "Soy tu script de consulta";
+	/* Variable: Espacio reservado en la memoria (temporalmente) cuyo valor puede campo */
+	
+	//php 		//html
+	$nombre 	= $_POST['nombre']; //cristian
+	$email 		= $_POST['email']; //cristiandracedo@hotmail.com
+	$consulta 	= $_POST['consulta']; //Buenas noches, América!!
+	/*Estructura del Mensaje*/
+	$mensaje = "
+		<strong>Nombre: </strong>$nombre <br />
+		<strong>Correo: </strong>$email <br />
+		<strong>Consulta: </strong>$consulta
+	";
+	/*Generacion automatica del Mensaje*/
+	mail(
+		"info@misitio.com", //Destinatario
+		"Consulta Web", //Asunto
+		$mensaje, // Cuerpo del Mail
+		"From:$nombre<$email>
+		\n Reply-To:$email
+		\n Content-Type: text/html; charset=utf-8" //Remitente - extras
+	);
+
+	header("location: https://misitio.com");
  ?>
