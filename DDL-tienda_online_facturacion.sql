46ecefeac1987d6f0cf5a4241d0a52f3491c5c04
C215714N <CristianDRacedo@hotmail.com>
2020-09-30 22:10:58 -0300

    Consulta HAVING

--- DDL-tienda_online_facturacion.sql
+++ DDL-tienda_online_facturacion.sql
@@ -29,6 +29,51 @@ CREATE TABLE facturacion(
     UNIQUE KEY (tipo, categoria, monto, id_cliente, fecha),
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
+SELECT * FROM facturacion;
+INSERT INTO facturacion(tipo, categoria,id_cliente,fecha)
+	VALUES 
+		(2,1,5,'2020-09-23,14:00'),
+		(2,2,1,'2020-09-23,15:00'),
+        (1,7,4,'2020-09-30,19:00');
+
+SELECT * FROM facturacion_detalle;
+INSERT INTO facturacion_detalle(id_factura, id_producto, cantidad)
+	VALUES
+		(1,3,1),
+        (1,4,2),
+        (1,7,10),
+        (1,6,2),
+        (1,5,5);
+## actualizacion de precios (factuacion_detalle)
+	UPDATE facturacion_detalle
+		SET precio = (
+			SELECT precio 
+			FROM productos
+			-- tabla.campo = tabla.campo (coincidencia segun id_producto)
+			WHERE facturacion_detalle.id_producto = productos.id_producto
+		);
+
+UPDATE facturacion
+	SET 
+		monto = 302095,
+        impuesto = 'iva',
+        porcentaje = 21,
+        bonificacion = 0
+	WHERE id_factura = 1;
+
+SELECT * from Facturacion;
+
+SELECT 
+	id_producto, 
+	SUM(cantidad * precio) AS total
+FROM facturacion_detalle
+GROUP BY id_factura;
+SELECT * FROM facturacion_detalle;
+SELECT id_producto, marca, modelo FROM productos;
+SELECT id_cliente, nombre, apellido 
+FROM clientes
+WHERE apellido LIKE 'B%';
+
 
 SELECT * FROM facturacion;
 CREATE TABLE facturacion_detalle (
