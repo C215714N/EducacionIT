65bef845c98ae4f907fb05f8358cfd2928f5ca08
c215714n <cristiandracedo@hotmail.com>
2020-11-11 11:34:29 -0300

    Media Queries

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.querySelector('#menu').addEventListener(
+	'click', () => {
+		document.querySelector('.menu').classList.toggle('openMenu');
+		if(document.querySelector('.menu').classList.contains('openMenu')){
+			document.querySelector('#menu').innerHTML = '&times;'
+		} else {
+			document.querySelector('#menu').innerHTML = '&equiv;'
+		}
+	}
+)
