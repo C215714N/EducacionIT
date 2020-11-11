a2555d6de3612284f1d04c0de6b71d5da903d2dc
c215714n <cristiandracedo@hotmail.com>
2020-11-11 13:31:29 -0300

    Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
-document.querySelector('#menu').addEventListener(
-	'click', () => {
-		document.querySelector('.menu').classList.toggle('openMenu');
-		if(document.querySelector('.menu').classList.contains('openMenu')){
-			document.querySelector('#menu').innerHTML = '&times;'
-		} else {
-			document.querySelector('#menu').innerHTML = '&equiv;'
+/*Menu Hamburguesa*/
+	document.querySelector('#menu').addEventListener(
+		'click', () => {
+			document.querySelector('.menu').classList.toggle('openMenu');
+			if(document.querySelector('.menu').classList.contains('openMenu')){
+				document.querySelector('#menu').innerHTML = '&times;'
+			} else {
+				document.querySelector('#menu').innerHTML = '&equiv;'
+			}
 		}
-	}
-)
+	)
+
+	let gallery = ['slide-1', 'slide-2', 'slide-3'];
+	gallery.map(item => document.getElementById(item).addEventListener(
+		'click', () => document.getElementById(item).classList.toggle('fixed'))
+	);
