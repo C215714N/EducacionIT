244d03205d034e2367d140b724967b635193eb98
c215714n <cristiandracedo@hotmail.com>
2020-11-16 13:16:39 -0300

    Boostrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+//Menu Hamburguesa
+	document.querySelector('#menu').addEventListener(
+		'click', () => {
+			document.querySelector('.menu').classList.toggle('openMenu');
+			if(document.querySelector('.menu').classList.contains('openMenu')){
+				document.querySelector('#menu').innerHTML = '&times;'
+			} else {
+				document.querySelector('#menu').innerHTML = '&equiv;'
+			}
+		}
+	)
+
 // galeria slide
 	let galeria = document.querySelectorAll('.galeria li');
 	//boton anterior
