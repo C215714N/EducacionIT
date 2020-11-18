897ed388e56b0258cade2de2ddf050e01e075fb7
c215714n <cristiandracedo@hotmail.com>
2020-11-18 13:24:51 -0300

    canvas & svg

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 /*Menu Hamburguesa*/
 	document.querySelector('#menu').addEventListener(
 		'click', () => {
@@ -10,7 +9,7 @@
 			}
 		}
 	);
-
+/*Galeria Slide*/
 /*videoSlide*/
 	//PREV
 
@@ -29,3 +28,26 @@
 			}
 		)
 	//NEXT
+/*ProgressBar*/
+	document.querySelector('#pInput').addEventListener(
+		'input', () => document.querySelector('#pBar').value = document.querySelector('#pInput').value
+	);
+/*Canvas*/
+	let ctx = document.querySelector('#canDraw').getContext('2d');
+	//rectangulo
+		ctx.fillStyle = "#248";
+		ctx.fillRect(50,50,100,100); //x y width height
+	//circulo
+		ctx.fillStyle = "rgba(240,32,64,.5)";
+		ctx.arc(150, 150, 50, 0, 2*Math.PI); //cx, cy, r, start, end
+		ctx.fill();
+	//poligono
+		ctx.fillStyle = "rgba(32,240,64,.5)";
+		ctx.beginPath();
+		ctx.moveTo(150,50);
+		ctx.lineTo(150,150);
+		ctx.lineTo(250, 100);
+		ctx.fill();
+	//texto
+		ctx.font = "20px sans-serif";
+		ctx.strokeText("JavaScript Bitmap", 50, 250);
