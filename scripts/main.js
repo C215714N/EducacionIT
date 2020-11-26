b601846aa613a5ca8d0936a9cdae6966b71948ec
c215714n <cristiandracedo@hotmail.com>
2020-11-25 22:15:01 -0300

    SVG & CANVAS

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,30 @@
 /*menu hamburguesa*/
 	let btnMenu = document.querySelector('#menu');
-	document.querySelector('#menu').addEventListener(
+	let ulMenu = document.querySelector('.menu');
+	
+	btnMenu.addEventListener(
 		'click', () => {
-			document.querySelector('.menu').classList.toggle('openMenu');
-			if(document.querySelector('.menu').classList.contains('openMenu')){
-				document.querySelector('#menu').innerHTML = '&times;'
+			ulMenu.classList.toggle('openMenu');
+			if(ulMenu.classList.contains('openMenu')){
+				btnMenu.innerHTML = '&times;'
 			}else{
-				document.querySelector('#menu').innerHTML = '&equiv;'
+				btnMenu.innerHTML = '&equiv;'
 			}
 		}
 	)
+/*Manipulacion elemento Progress*/
+	let pBar = document.querySelector('#pBar');
+	let pIn = document.querySelector('#pIn');
+
+	pIn.addEventListener(
+		'input', () => pBar.value = pIn.value
+	)
+/*Canvas*/
+	let ctx = document.querySelector('#jsCanvas').getContext('2d');
+	//Rectangulo
+		ctx.fillStyle = "#248";
+		ctx.fillRect(50,50,100,100);/*x, y, w, h*/
+	//Circulo
+		ctx.fillStyle = "rgba(192, 32, 64, .5)";
+		ctx.arc(150,150, 50, 0, 2*Math.PI);
+		ctx.fill();
