ac69f316036dd3657734f7b6c603700157b6e168
C215714N <CristianDRacedo@hotmail.com>
2021-01-28 20:40:04 -0300

    galeria slide

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
-	menuButton = document.querySelector('#menu')
-	menuList = document.querySelector('nav .menu')
+/* Menu Desplegable */
+	menuButton 	= document.querySelector('#menu')
+	menuList 	= document.querySelector('nav .menu')
 
-	menuButton.onclick = () => { 
-		menuList.classList.toggle('active') ? 
-			menuButton.innerHTML = `&times;` : 
-			menuButton.innerHTML = `&equiv;` 
-	}
+	menuButton.onclick = () => menuList.classList.toggle('active') ? menuButton.innerHTML = `&times;` : menuButton.innerHTML = `&equiv;`
+
+/* Galeria de Elementos */
+	prev	= document.querySelector('#header .prev')
+	next	= document.querySelector('#header .next')
+	imageList	= document.querySelectorAll('#header li')
+	spanList	= document.querySelectorAll('#header span')
+	// Eventos 
+		prev.onclick = () => changeItem()
+		next.onclick = () => changeItem()
+	// Obtener el elemento activo y cambiar
