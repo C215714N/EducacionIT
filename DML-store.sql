02cd7ccf46575dbc77098bde3a82ffa2ce34c956
Cristian Racedo <CristianDRacedo@Hotmail.com>
2021-02-02 00:16:41 -0300

    script DML

--- DML-store.sql
+++ DML-store.sql
@@ -3,3 +3,4 @@ USE store;
 	## carga selectiva
 	INSERT INTO clientes(apellido, nombre, fecha_nac, tipo_doc, num_doc) -- elegimos los campos
 		VALUES('Racedo', 'Cristian', '1991-04-18', 1, 35335335); -- valores
+	SELECT * FROM clientes;
