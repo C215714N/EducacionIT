caaeac6d9b9550c875e716ce83e2542af6952f1d
C215714N <CristianDRacedo@hotmail.com>
2021-02-02 11:59:26 -0300

    tablas productos y proveedores

--- DDL-store.sql
+++ DDL-store.sql
@@ -4,7 +4,7 @@
 	USE store; -- utilizar
 
 /*creacion de tablas*/
-	## clientes
+	## CLIENTES
     CREATE TABLE clientes(
 		id_cliente INT AUTO_INCREMENT, -- clave principal autonumerica
         apellido VARCHAR(50), -- texto variable
@@ -19,7 +19,40 @@
         PRIMARY KEY(id_cliente), -- campo relacional (vincula con otras tablas)
         UNIQUE KEY(tipo_doc, sexo, num_doc) -- campo/s unico/s local/es
     );
+	-- modificar tablas
+	ALTER TABLE clientes
+		ADD observaciones varchar(200),
+		DROP tel,
+		MODIFY sexo ENUM("m","f","o");
+	
+    ## PROVEEDORES
+	CREATE TABLE proveedores(
+		id_proveedor INT AUTO_INCREMENT,
+        proveedor VARCHAR(100),
+        cuil CHAR(13), -- 99-99999999-9
+        contacto VARCHAR(50),
+        tel VARCHAR(20),
+        direccion VARCHAR(100),
+		PRIMARY KEY(id_proveedor),
+        UNIQUE KEY(cuil)
+    );
+    
+    ## PRODUCTOS
+    DROP TABLE IF EXISTS productos;
+    CREATE TABLE productos(
+		id_producto INT AUTO_INCREMENT,
+        articulo VARCHAR(50),
+        codigo VARCHAR(20),
+        categoria ENUM('tecnologia', 'hogar', 'higiene', 'alimentos'),
+        stock INT NOT NULL,
+        precio INT NOT NULL,
+        valoracion ENUM("1","2","3","4","5"),
+        detalle VARCHAR(200),
+        id_proveedor INT NOT NULL,
+        PRIMARY KEY(id_producto),
+        UNIQUE KEY(codigo),
+        FOREIGN KEY(id_proveedor) REFERENCES proveedores(id_proveedor) -- relacion clave externa (campo_local) tabla_ext(campo_ext)
+	);
     
     SHOW TABLES; 
     DESCRIBE clientes;
-    SELECT * FROM clientes;
--- DML-store.sql
+++ DML-store.sql
@@ -1,6 +1,32 @@
 USE store;
 /*CARGA DE DATOS*/
+	## carga completa
+	INSERT INTO clientes
+    values (
+		0, 			-- id_cliente
+        "Racedo",	-- apellido
+        "Abel", 	-- nombre
+        1, 			-- tipo_doc (1: dni, 2: dni ext., 3: ci, 4: lc, 5: le, 6:pass)
+		1, 			-- sexo (1:m, 2:f)
+        38383838, 	-- num_doc
+        "1993-10-12",		-- fecha_nac (yyyy-mm-dd)
+        "(11)-0303-4560",	-- tel (texto)
+        "abel.racedo@gmail.com",	-- email
+        "su casa 93, Temperley"		-- direccion
+        );
+        
 	## carga selectiva
 	INSERT INTO clientes(apellido, nombre, fecha_nac, tipo_doc, num_doc) -- elegimos los campos
 		VALUES('Racedo', 'Cristian', '1991-04-18', 1, 35335335); -- valores
+	SELECT * FROM clientes;
+    
+    ## carga multiple
+	INSERT INTO clientes(apellido, nombre, sexo, tipo_doc, num_doc)
+		VALUES 
+			("Gomez","Gonzalo", 1, 3, 27272727),
+			("Lopez","Lorena", 2, 4, 32323232),
+            ("Perez","Perla", 2, 1, 19191919),
+            ("Diaz","Diana", 1, 2, 38383838),
+            ("Gimenez","Gerardo", 1, 5, 35353535);
+            
 	SELECT * FROM clientes;
