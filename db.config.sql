3f0c00b366dbf9bdc74c3a3a5b97136d1fb865d6
Jonathan Kim <jonathankim86@gmail.com>
2021-02-05 16:00:23 -0300

    Conexión con el servidor

---
+++ db.config.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS Delilah;
+USE Delilah;
+
+CREATE TABLE Usuarios ();
+CREATE TABLE Productos ();
+CREATE TABLE Pedidos ();
+CREATE TABLE Pedidos_Detalle ();
---
+++ server.js
@@ -0,0 +1,24 @@
+/* Consts */
+const express = require('express'); // Para poder establecer la comunicación.
+const bodyParser = require('body-parser'); // Para la conversión de los datos.
+const delilah = express(); // La aplicación mediante Express.
+const port = process.env.PORT || 5000; // Puerto de la aplicación (Postman o 5000).
+
+/*Acceso a la aplicación */
+delilah.use(
+    bodyParser.urlencoded({extended : true}) // urlencoded: a través de la URL reciba la información solicitada.
+);
+
+delilah.use(
+    bodyParser.json() // Convierte los datos recibidos en formato .json.
+);
+
+delilah.get('/', (req, res) => {
+    res.send('La conexión funciona correctamente.') // Mensaje de conexión exitosa.
+});
+
+delilah.listen(port, () => {
+    console.log(`El puerto utilizado es: ${port}.`); // Notificación del puerto.
+});
+
+/* Ejecutar el comando "node server" o "npm start" para ejecutar el servidor local*/
