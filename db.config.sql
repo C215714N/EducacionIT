a48c2351b3ba83e63c8f5dfa769349ae0b8cd1e2
Jonathan Kim <jonathankim86@gmail.com>
2021-02-09 16:02:48 -0300

    Inserción de datos

--- db.config.sql
+++ db.config.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS Delilah;
 USE Delilah;
 
-CREATE TABLE Usuarios (
+CREATE TABLE IF NOT EXISTS Usuarios (
     id_usuario int auto_increment,
     nombre varchar(50) not null,
     usuario varchar(20) not null,
@@ -15,11 +15,12 @@ CREATE TABLE Usuarios (
     unique key(usuario),
     unique key(correo)
 );
-CREATE TABLE Productos (
+
+CREATE TABLE IF NOT EXISTS Productos (
 	id_producto int auto_increment,
     nombre varchar(50) not null,
     descripcion varchar(200),
-    precio decimal(4,2) not null,
+    precio decimal(8,2) not null,
     stock int,
     imagen varchar(200),
     fecha datetime default current_timestamp,
@@ -27,41 +28,84 @@ CREATE TABLE Productos (
     unique key(nombre)
 );
 
-CREATE TABLE Estados (
+CREATE TABLE IF NOT EXISTS Estados (
 	id_estado int auto_increment,
     detalle varchar(50),
     primary key(id_estado),
     unique key(detalle)
 );
 
-CREATE TABLE Tipos_pago (
+CREATE TABLE IF NOT EXISTS Tipos_pago (
 	id_tipo int auto_increment,
     detalle varchar(50),
     primary key(id_tipo),
     unique key(detalle)
 );
 
-CREATE TABLE Pedidos (
+CREATE TABLE IF NOT EXISTS Pedidos (
 	id_pedido int auto_increment,
     id_usuario int not null,
     id_estado int not null, 
     id_tipo int,
     fecha datetime default current_timestamp,
-    precio decimal(4,2) not null,
+    precio decimal(8,2) not null,
     pago boolean default false,
     primary key(id_pedido),
-    foreign key(id_usuario) references Usuarios(id_usuarios),
+    foreign key(id_usuario) references Usuarios(id_usuario),
     foreign key(id_estado) references Estados(id_estado),
     foreign key(id_tipo) references Tipos_pago(id_tipo)
 );
-CREATE TABLE Pedidos_detalle (
+CREATE TABLE IF NOT EXISTS Pedidos_detalle (
 	id_detalle int auto_increment,
     id_pedido int,
     id_producto int,
     cantidad int,
-    precio decimal(4,2) not null,
+    precio decimal(8,2) not null,
     primary key(id_detalle),
     unique key(id_pedido, id_producto),
     foreign key(id_pedido) references Pedidos(id_pedido),
     foreign key(id_producto) references Productos(id_producto)
 );
+
+/*
+DDL (definición de datos: columnas): create, drop y alter (table) [add, drop y modify].
+DML (manipulación de datos: filas): insert (into), delete, update [set] y select.
+DCL (control de datos: usuarios de la base de datos): grant, revoke, flush privileges.
+TCL (transacción de datos): describe, show, use, entre otros...
+*/
+
+DESCRIBE Usuarios;
+INSERT INTO Usuarios(usuario, correo, pass, administrator)
+VALUES
+	("freddi_mercury", "freddimercury@gmail.com", "queen", false),
+	("jonathan_kim", "jonathankim@gmail.com", "jonathan", true);
+
+SELECT * FROM Usuarios;
+UPDATE Usuarios SET nombre = "Farrokh Bulsara" WHERE id_usuario = 1;
+UPDATE Usuarios SET nombre = "Jonathan Kim" WHERE correo = "jonathankim@gmail.com";
+DELETE FROM Usuarios WHERE id_usuario = 2;
+INSERT INTO Usuarios(usuario, correo, pass, administrator)
+VALUES
+	("cristian_racedo2", "cristianracedo2@gmail.com", "vtodyosm", false),
+    ("jonathan_kim2", "jonathankim2@gmail.com", "jonathan2", false);
+    
+TRUNCATE Usuarios;
+
+DESCRIBE Productos;
+INSERT INTO Productos(nombre, precio, stock)
+VALUES
+	("Hamburguesa clásica","350","563"),
+    ("Sandwich veggie","310","321");
+
+SELECT * FROM Productos;
+
+ALTER TABLE Pedidos_detalle
+MODIFY precio DECIMAL (8,2);
+
+/* CASE siempre termina con END */
+UPDATE Productos
+	SET precio =
+    CASE
+		WHEN nombre LIKE "Hamb%" THEN 350
+        WHEN nombre LIKE "Sand%" THEN 310
+	END
