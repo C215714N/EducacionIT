2b33e6abd8953ab1dba5f49a287d8fd406bffdbf
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-04-07 16:11:47 -0300

    variables css

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,44 @@
+/*Menu desplegable*/
+	menuBtn = document.querySelector('nav .button');
+	menuLst = document.querySelector('nav .menu');
+
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+/*Galeria de Imágenes*/
+	// Variables
+	prev = document.querySelector('#header .prev');
+	next = document.querySelector('#header .next');
+	bgList = document.querySelectorAll('#header .slide li')
+	spList = document.querySelectorAll('#header .slide span')
+	// Intervalo de 25s
+	window.setInterval(() => next.click(), 25000 )
+	// Eventos
+	prev.onclick = () => { 
+		changeItem(bgList, false)
+		changeItem(spList, false)  
+	}
+	next.onclick = () => { 
+		changeItem(bgList, true)
+		changeItem(spList, true)  
+	}
+	// Funcion de Cambio de elemento
+	const changeItem = (itemList, type) => {
+		for ( i = 0 ; i < itemList.length ; i++ ) {
+			if(itemList[i].classList.contains('active')){
+				itemList[i].classList.remove('active')
+				switch(type){
+					case true:
+						( i < itemList.length - 1 ) ? newItem = itemList[i + 1] : newItem = itemList[0]
+					break;
+					default:
+						( i > 0) ? newItem = itemList[i - 1] : newItem = itemList[itemList.length - 1]
+		}	}	} 
+		newItem.classList.add('active')
+	}
 /* ProgressBar */
 	ProIn  = document.querySelector('#jsProgress input')
 	ProBar = document.querySelector('#jsProgress progress')
 
-	ProIn.addEventListener(	'input', () => ProBar.value = ProIn.value )
+	ProIn.addEventListener( 'input', () => ProBar.value = ProIn.value )
 /* Canvas */
 	ctx = document.querySelector('#canvas').getContext('2d')
 	// Rectangulo
