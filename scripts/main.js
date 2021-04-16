358540de68a2e3a04a85c83c7d53e6ac774b17d6
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-04-15 22:12:19 -0300

    bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,29 @@
 		// valor si NO se cumple
 			menuBtn.innerHTML = '&equiv;'
 	}
+
+// Galeria slide
+	slideLst = document.querySelectorAll('.slide li')
+	prev = document.querySelector('#header .icon-prev')
+	next = document.querySelector('#header .icon-next')
+
+	prev.onclick = () => changeItem(slideLst)
+	next.onclick = () => changeItem(slideLst, true)
+
+	const changeItem = (itemList, type) => {
+		for (i = 0; i < itemList.length ; i++ ){
+			if (itemList[i].classList.contains('active')){
+				itemList[i].classList.remove('active')
+				switch (type){
+					case true:
+						( i < itemList.length - 1 ) ? 
+						newItem = itemList[i + 1] :
+						newItem = itemList[0] 
+					break;
+					default:
+						( i > 0 ) ?
+						newItem = itemList[i - 1] :
+						newItem = itemList[itemList.length - 1]
+		}	}	}
+		newItem.classList.add('active')
+	}
