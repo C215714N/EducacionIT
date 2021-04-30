d29901c8ad1c748426c57f75e5dea34f86311266
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-04-30 18:02:42 -0300

    header slides

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,30 @@
-menuBtn = document.querySelector('nav .button')
-menuLst = document.querySelector('nav .menu')
+// navigation
+	menuBtn = document.querySelector('nav .button')
+	menuLst = document.querySelector('nav .menu')
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 
-menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+// slide
+	slideLst = document.querySelectorAll('#header .slide li')
+	prev = document.querySelector('#header .icon-prev')
+	next = document.querySelector('#header .icon-next')
+
+	prev.onclick = () => changeItem(slideLst, false)
+	next.onclick = () => changeItem(slideLst, true)
+
+	const changeItem = (itemList, type) => {
+		for(i = 0; i < itemList.length; i++){
+			if(itemList[i].classList.contains('active')){
+				itemList[i].classList.remove('active')
+				switch(type){
+					case true:
+						i < itemList.length - 1 ? 
+							newItem = itemList[i + 1] : 
+							newItem = itemList[0]
+					break;
+					default:
+						i > 0 ?	
+							newItem = itemList[i - 1] : 
+							newItem = itemList[itemList.length - 1]
+		}	}	}
+		newItem.classList.add('active')
+	}
