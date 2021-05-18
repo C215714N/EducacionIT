a57573f137c3c8c04c1a81d6daf3425faf12be05
Cristian Racedo <cristiandracedo@hotmail.com>
2021-05-18 12:16:49 -0300

    Table - Products

--- DDL-ecommerce.sql
+++ DDL-ecommerce.sql
@@ -12,12 +12,24 @@
 	DROP TABLE IF EXISTS users;
 	CREATE TABLE users(
 		user_id INT AUTO_INCREMENT, -- numerico "automatico"
-		user_name VARCHAR(20), -- caracter variable max(20)
-        user_email VARCHAR(100),
+		user_name VARCHAR(20) NOT NULL, -- caracter variable max(20)
+        user_email VARCHAR(100) NOT NULL, -- almacena un valor
 		user_pass VARCHAR(20),
 		PRIMARY KEY(user_id), -- Clave Principal (campo relacional)
 		UNIQUE KEY(user_name), -- Clave Unica (campo local)
 		UNIQUE KEY(user_email)
    );
-    
-    DESCRIBE users;
+   CREATE TABLE categories(
+		cat_id INT AUTO_INCREMENT,
+        description VARCHAR(50),
+        PRIMARY KEY(cat_id),
+        UNIQUE KEY(description)
+   );
+   CREATE TABLE products (
+		product_id INT AUTO_INCREMENT,
+        description VARCHAR(200) NOT NULL,
+        -- category ENUM("alimentos", "higiene", "hogar", "tecnologia"),
+        category INT,
+        PRIMARY KEY(product_id),
+        FOREIGN KEY (category) REFERENCES categories(cat_id)
+   ); 
--- DML-ecommerce.sql
+++ DML-ecommerce.sql
@@ -1,11 +1,26 @@
 /* DML (Data Manipulation Language) */
 ## Insercion de datos
-	INSERT INTO users
+	-- completa
+	INSERT INTO users 
     VALUES (0,"cristian_racedo","cristiandracedo@hotmail.com","root");
-    
+    -- selectiva
     INSERT INTO users(user_email, user_name, user_pass)
-    VALUES ("cristiandracedo@hotmail.com", "c215714n", "root");
+    VALUES ("cristiandracedo@gmail.com", "c215714n", "root");
+    -- multiple
+    INSERT INTO users(user_name, user_email)
+    VALUES 
+		("a_santagada","a_santagada@gmail.com"),
+        ("f_cichon","f_cichon"),
+        ("f_varela","f_varela"),
+        ("c_fernandez","c_fernandez");
     
 ## Consulta de Datos
 	SELECT * FROM users;
     
+## Actualizacion 
+	UPDATE users SET user_pass = "root" 
+    WHERE user_name = "cristian_racedo";
+    
+## Eliminacion de registros
+    DELETE FROM users; -- elimina todos los registros
+    TRUNCATE users; -- elimina registros y reinicia los contadores de la tabla 
