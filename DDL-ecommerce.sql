3b7d566862d233d676dac950431e3a52afa95ec4
Cristian Racedo <cristiandracedo@hotmail.com>
2021-05-18 11:10:47 -0300

    DML - ecommerce

--- DDL-ecommerce.sql
+++ DDL-ecommerce.sql
@@ -3,14 +3,21 @@
 	DROP DATABASE IF EXISTS ecommerce; -- elimina db
 	CREATE DATABASE IF NOT EXISTS ecommerce; -- crear db
 	USE ecommerce; -- selecciona la db	
+/*
+	users => publish
+    products => buy
+    accounting => user => publish
+*/
 ## Creacion de tablas
+	DROP TABLE IF EXISTS users;
 	CREATE TABLE users(
-		user_id INT AUTO_INCREMENT,
-		username VARCHAR(50),
-		userpass VARCHAR(20),
-		PRIMARY KEY(user_id),
-		UNIQUE KEY(username)
-    );
+		user_id INT AUTO_INCREMENT, -- numerico "automatico"
+		user_name VARCHAR(20), -- caracter variable max(20)
+        user_email VARCHAR(100),
+		user_pass VARCHAR(20),
+		PRIMARY KEY(user_id), -- Clave Principal (campo relacional)
+		UNIQUE KEY(user_name), -- Clave Unica (campo local)
+		UNIQUE KEY(user_email)
+   );
     
-    
-    
+    DESCRIBE users;
---
+++ DML-ecommerce.sql
@@ -0,0 +1,11 @@
+/* DML (Data Manipulation Language) */
+## Insercion de datos
+	INSERT INTO users
+    VALUES (0,"cristian_racedo","cristiandracedo@hotmail.com","root");
+    
+    INSERT INTO users(user_email, user_name, user_pass)
+    VALUES ("cristiandracedo@hotmail.com", "c215714n", "root");
+    
+## Consulta de Datos
+	SELECT * FROM users;
+    
