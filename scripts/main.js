1efc8c8c6bbd160a1286542a3c6dc94c01985cfa
Cristian Racedo <cristiandracedo@hotmail.com>
2021-05-26 13:12:26 -0300

    Progress Bar

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,8 @@
 	menuLst = document.querySelector('nav .menu'); 
 
 	menuBtn.onclick = () =>	menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+/*Progress bar*/
+	pBar = document.querySelector('.progress progress')
+	pIn = document.querySelector('.progress input')
+
+	pIn.addEventListener('input', () => pBar.value = pIn.value)
