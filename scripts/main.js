0ec9d189f24a5f894af4abd52f37a075d78272fd
Cristian Racedo <cristiandracedo@hotmail.com>
2021-06-12 12:56:07 -0300

    elementos html5

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+
+
+// Progress bar
+	pBar = document.querySelector('.progress progress')
+	pIn  = document.querySelector('.progress input')
+	pIn.addEventListener('input', () => pBar.value = pIn.value)
