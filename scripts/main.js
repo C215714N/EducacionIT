8b59b4cbf452a2719b8a37c8b8ffc41f76917737
Cristian Racedo <cristiandracedo@hotmail.com>
2021-06-14 20:51:40 -0300

    galeria con transiciones

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,29 @@
 	pIn  = document.querySelector('.progress input')
 
 	pIn.addEventListener('input', () => pBar.value = pIn.value )
+/* Gallery */
+	// Variables
+	spanLst = document.querySelectorAll('#header span')
+	slideLst = document.querySelectorAll('#header li')
+	prev = document.querySelector('#header .icon-prev')
+	next = document.querySelector('#header .icon-next')
+	//Eventos
+	prev.onclick = () => { changeItem(slideLst); changeItem(spanLst) }
+	next.onclick = () => { changeItem(slideLst, true); changeItem(spanLst, true) }
+	//Funciones
+	const changeItem = (itemList, type) => {
+		for(i = 0; i < itemList.length; i++){
+			if(itemList[i].classList.contains('active')){
+				itemList[i].classList.remove('active')
+				switch(type){
+					case true:
+						i < itemList.length - 1 ?
+							newItem = itemList[i + 1] :
+							newItem = itemList[0]
+					break;
+					default:
+						i > 0 ?
+							newItem = itemList[i - 1] :
+							newItem = itemList[ itemList.length - 1]
+		}	}	} newItem.classList.add('active')
+	}
