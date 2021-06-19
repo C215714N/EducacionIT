9edeb3121fa4a0aa2ff98f0d347d549cda00ce9b
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-06-19 12:02:02 -0300

    lesscss

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
-
+// Navigation Menu
+	menuBtn = document.querySelector('nav .button')
+	menuLst = document.querySelector('nav .menu')
+	
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 
 // Progress bar
 	pBar = document.querySelector('.progress progress')
