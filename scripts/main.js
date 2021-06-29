f2927ccf0a90e6e5ebc4f23585e0eeb9e192ea7f
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-06-29 12:51:38 -0300

    Navigation Bar

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+/*Navigation*/
+	menuBtn = document.querySelector('nav .btn')
+	menuLst = document.querySelector('nav .menu')
+	
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
