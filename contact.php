430841fa26f43643e6bb7f553d04632fb7b2ac5e
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-06-30 01:46:35 -0300

    Reorganizacion de Estilos

--- contact.php
+++ contact.php
@@ -1,15 +1,14 @@
 <?php  
-	$nombre 	= $_POST['nombre'];
-	$email 		= $_POST['email'];
-	$tipo 		= $_POST['tipo'];	
-	$consulta	= $_POST['consulta'];
-	$mensaje	= "
+	$nombre   = $_POST['nombre'];
+	$email 	  = $_POST['email'];
+	$tipo 	  = $_POST['tipo'];	
+	$consulta = $_POST['consulta'];
+	$mensaje  = "
 		<strong> Nombre: </strong> $nombre <br/>
 		<strong> Correo: </strong> $email <br/>
 		<strong> Consulta: </strong> $consulta
 	";	
-	
-	/*mail(
+	mail(
 		"CristianDRacedo@hotmail.com",
 		$tipo,	
 		$mensaje,
@@ -17,10 +16,7 @@
 		\n Reply-To: $email
 		\n Content-Type: text/html;
 		\n charset=utf-8"
-	);*/
-
-	echo($mensaje);
-	
+	);
 	header(
 		"location: https://c215714n.github.io/EducacionIT"
 	);
