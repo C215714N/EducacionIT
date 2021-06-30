45c7710a5e4e7a1e127ebc375b5744b3828ce235
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-06-30 13:25:02 -0300

    Ultimos cambios

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,28 @@
 	menuLst = document.querySelector('nav .menu')
 	
 	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+/*Slide Gallery*/
+	slideLst = document.querySelectorAll('.slide li')
+	spanLst = document.querySelectorAll('.header span')
+	prev = document.querySelector('#header .icon-prev')
+	next = document.querySelector('#header .icon-next')
+
+	prev.onclick = () => {changeItem(slideLst); changeItem(spanLst) }
+	next.onclick = () => {changeItem(slideLst, true); changeItem(spanLst, true)}
+
+	const changeItem = (itemList, type) => {
+		for(i=0; i < itemList.length; i++){
+			if(itemList[i].classList.contains('active')){
+				itemList[i].classList.remove('active')
+				switch(type){
+					case true:
+						i < itemList.length - 1 ?
+							newItem = itemList[i + 1] :
+							newItem = itemList[0]
+					break;
+					default:
+						i > 0 ?
+							newItem = itemList[i - 1] :
+							newItem = itemList[ itemList.length - 1]
+			}	}	} newItem.classList.add('active')
+	}
