82b1ff03d26ade81e6302cb4cb209cf43a40f6ec
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-07-03 10:52:59 -0300

    Logica Ampliacion de mapa

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,9 @@
 	pBar = document.querySelector('.progress progress')
 	pIn  = document.querySelector('.progress input')
 	pIn.addEventListener('input', () => pBar.value = pIn.value)
+
+// Google Maps
+	map = document.querySelector('#footer iframe')
+	mapBtn = document.querySelector('#footer .button.opacity')
+
+	mapBtn.onclick= () => map.classList.toggle('active') ? mapBtn.classList.replace('icon-max', 'icon-min') : mapBtn.classList.replace('icon-min', 'icon-max')
