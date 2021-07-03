17a18f1fecb8ba458088d5db2528918f0f8d1d48
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-07-03 11:32:15 -0300

    Galeria Slide

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,26 @@
 	mapBtn = document.querySelector('#footer .button.opacity')
 
 	mapBtn.onclick= () => map.classList.toggle('active') ? mapBtn.classList.replace('icon-max', 'icon-min') : mapBtn.classList.replace('icon-min', 'icon-max')
+
+// Slide Gallery
+	prev = document.querySelector('#header .icon-prev')
+	next = document.querySelector('#header .icon-next')
+	slideLst  = document.querySelectorAll('#header li')
+	spanLst = document.querySelectorAll('#header span')
+
+	window.setInterval(() => next.click(), 15000)
+	prev.onclick = () => { changeItem(slideLst); changeItem(spanLst) }
+	next.onclick = () => { changeItem(slideLst, true); changeItem(spanLst, true) }
+
+	const changeItem = (itemList, type) => {
+		for (i = 0; i < itemList.length; i++){
+			if (itemList[i].classList.contains('active')){
+				itemList[i].classList.remove('active')
+				switch(type){
+					case true:
+						i < itemList.length - 1 ? newItem = itemList[i + 1] : newItem = itemList[0]
+					break;
+					default:
+						i > 0 ? newItem = itemList[i - 1] :	newItem = itemList[itemList.length - 1]
+		}	}	}	newItem.classList.add('active')
+	}
