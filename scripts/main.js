c1acacb39c0a5de0215dfd6b42c09cfef639c9cd
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-07-05 11:01:04 -0300

    Scripts: Navigation Bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
+/*Navigation Bar*/
+	menuBtn = document.querySelector('nav .btn')
+	menuLst = document.querySelector('nav .menu')
 
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 /*Elemento Progress*/
 	pBar = document.querySelector('.progress progress')
 	pIn  = document.querySelector('.progress input')
