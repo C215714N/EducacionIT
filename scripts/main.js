49922db5fbeea91bb26c0d15f89048f09d40ba98
Cristian Racedo <cristiandracedo@hotmail.coom>
2021-07-19 10:53:24 -0300

    Logica: Galeria de Imagenes

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,29 @@
 	pIn  = document.querySelector('.progress input')
 	
 	pIn.addEventListener('input', () => pBar.value = pIn.value)
+/*Galeria de Imagenes*/
+	prev = document.querySelector('#header .icon-prev');
+	next = document.querySelector('#header .icon-next');
+	slideLst = document.querySelectorAll('#header .slide li')
+	spanLst = document.querySelectorAll('#header .header span')
+
+	window.setInterval( () => next.click() , 30000)
+	prev.onclick = () => { changeItem(slideLst); changeItem(spanLst) }
+	next.onclick = () => { changeItem(slideLst, true); changeItem(spanLst, true) }
+
+	const changeItem = (array, type, className = 'active') => {
+		for(i = 0; i < array.length ; i++){
+			if(array[i].classList.contains(className)){
+				array[i].classList.remove(className)
+				switch(type){
+					case true:
+						i < array.length - 1 ?
+							newItem = array[i + 1] :
+							newItem = array[0]
+					break;
+					default:
+						i > 0 ?
+							newItem = array[i - 1] :
+							newItem = array[array.length - 1]
+		}	}	}	newItem.classList.add(className)	
+	}
