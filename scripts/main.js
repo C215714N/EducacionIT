1bfaf53d5459acada264b4d6c964f1cc554b2d6d
Cristian Racedo <cristiandracedo@hotmail.com>
2021-08-20 14:36:33 -0300

    Navegacion Responsive

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,7 @@
 	menuBtn = document.querySelector('nav .btn')
 	menuLst = document.querySelector('nav .menu')
 
-	menuBtn.onclick = () => menuLst.classList.toggle('active')
+	menuBtn.onclick = () => 
+		menuLst.classList.toggle('active') ? 
+			menuBtn.innerHTML = '&times;' : 
+			menuBtn.innerHTML = '&equiv;'
