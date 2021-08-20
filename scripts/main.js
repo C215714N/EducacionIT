ce2306d46b31956e9c849b9ad553dcf7dcfe2d16
Cristian Racedo <cristiandracedo@hotmail.com>
2021-08-20 16:46:34 -0300

    Pseudoelementos

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,29 @@
 		menuLst.classList.toggle('active') ? 
 			menuBtn.innerHTML = '&times;' : 
 			menuBtn.innerHTML = '&equiv;'
+/*Galeria de Imagenes*/
+	prev = document.querySelector('#header .icon-prev')
+	next = document.querySelector('#header .icon-next')
+	slide = document.querySelectorAll('.slide li')
+
+	prev.onclick = () => changeItem(slide)
+	next.onclick = () => changeItem(slide, true)
+
+	const changeItem = (array, type) => {
+		for(i = 0; i < array.length; i++){
+			if( array[i].classList.contains('active')){
+				array[i].classList.remove('active')
+				switch(type){
+					case true:
+						i < array.length - 1 ?
+							newItem = array[i + 1] :
+							newItem = array[0]
+					break;
+					default:
+						i > 0 ?
+							newItem = array[i - 1] :
+							newItem = array[array.length - 1]
+				}	
+			}
+		} newItem.classList.add('active')
+	}
