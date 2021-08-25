66f1c2bb9aa9c09a77c8f17f43cddb42938b74e3
Cristian Racedo <cristiandracedo@hotmail.com>
2021-08-25 10:24:55 -0300

    Estilos: Navegacion Responsive

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 	menuBtn = document.querySelector('nav .btn')
 	menuLst = document.querySelector('nav .menu')
 
-	menuBtn.onclick = () => menuLst.classList.toggle('active')
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 /*Progress Bar*/
 	pBar = document.querySelector('.progress progress')
 	pIn  = document.querySelector('.progress input')
