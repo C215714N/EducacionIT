5b4a823773437adba7ea7e800af987c0cbe6143d
Cristian Racedo <cristiandracedo@hotmail.com>
2021-08-25 09:43:23 -0300

    Logica: Menu Navegacion

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+/*Navigation Bar*/
+	menuBtn = document.querySelector('nav .btn')
+	menuLst = document.querySelector('nav .menu')
+
+	menuBtn.onclick = () => menuLst.classList.toggle('active')
+/*Progress Bar*/
+	pBar = document.querySelector('.progress progress')
+	pIn  = document.querySelector('.progress input')
+
+	pIn.addEventListener( 'input', () => pBar.value = pIn.value )
