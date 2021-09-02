40e7496b22c453821b90e4d9d4f4db19c3179b7b
Cristian Racedo <cristiandracedo@hotmail.com>
2021-09-02 12:44:50 -0300

    Mapa Pantalla Completa

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+/*Mapa de contacto*/
+	mapBtn 	= document.querySelector('#footer .map')
+	mapFrm 	= document.querySelector('#footer .cover')
+
+	mapBtn.onclick = () => mapFrm.classList.toggle('active') ? mapBtn.classList.replace('max', 'min') : mapBtn.classList.replace('min', 'max') 
 /*Mostrar elementos*/
 	showBtn = document.querySelectorAll('.show')
 	layouts = document.querySelectorAll('.layout')
