83e02147e43255266b6abd97cc0748b8abb41bed
Cristian Racedo <cristiandracedo@hotmail.com>
2021-09-02 22:13:19 -0300

    Logica Navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+/*Navegacion Principal*/
+	menuBtn  = document.querySelector('nav .btn')
+	menuLst  = document.querySelector('nav .menu')
+
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 /*Mostrar contenido*/
 	showBtn  = document.querySelectorAll('.show')
 	layouts  = document.querySelectorAll('.layout')
@@ -7,7 +12,7 @@
 		btn.onclick = () => layouts[i].classList.toggle('active') ? showSpan[i].innerHTML = '(ocultar)' : showSpan[i].innerHTML = '(mostrar)' 
 	} )
 /*Mapa de Contacto*/
-	mapBtn = document.querySelector('#footer .map')
-	mapFrm = document.querySelector('#footer .cover')
+	mapBtn  = document.querySelector('#footer .map')
+	mapFrm  = document.querySelector('#footer .cover')
 
 	mapBtn.onclick = () => mapFrm.classList.toggle('active') ? mapBtn.classList.replace('icon-max','icon-min') : mapBtn.classList.replace('icon-min', 'icon-max')
