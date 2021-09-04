0daba6c46e72e569b3499c4ba4f9be1409219ee1
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-04 17:00:58 -0300

    DDL - Creacion DB

---
+++ DDL-ventapps.sql
@@ -0,0 +1,5 @@
+/*DDL - Data Definition Language */
+	## Crear Base Datos
+    DROP DATABASE IF EXISTS ventapps; -- eliminar si existe db
+    CREATE DATABASE ventapps; -- nombre db
+    USE ventapps; -- seleccionamos db
