92ccd0a02dcb10d04c26f861ec6d220c0f50fa49
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-08 10:55:27 -0300

    Galeria de Imagenes

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,47 @@
+	const d = document
 /*Navigation Bar*/
-	menuBtn = document.querySelector('nav .btn')
-	menuLst = document.querySelector('nav .menu')
+	menuBtn = d.querySelector('nav .btn')
+	menuLst = d.querySelector('nav .menu')
 
 	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 /*Progress Bar*/
-	pBar = document.querySelector('.progress progress')
-	pIn  = document.querySelector('.progress input')
+	pBar = d.querySelector('.progress progress')
+	pIn  = d.querySelector('.progress input')
 
 	pIn.addEventListener( 'input', () => pBar.value = pIn.value )
+
+/*Gallery*/
+	prev = d.querySelectorAll('.icon-prev')
+	next = d.querySelectorAll('.icon-next')
+	spanLst = d.querySelectorAll('.control')
+	slideLst = d.querySelectorAll('.slide')
+
+	// prev.onclick = () => { changeItem(slideLst); changeItem(spanLst) }
+	// next.onclick = () => { changeItem(slideLst, true); changeItem(spanLst, true) }
+	window.setInterval( () => next[0].click(), 10000 )
+	prev.forEach( (p,i) => p.onclick = () => {
+		getArray(i)
+		changeItem(spn)
+		changeItem(img)
+	}	)
+	next.forEach( (n,i) => n.onclick = () => {
+		getArray(i)
+		changeItem(spn, true)
+		changeItem(img, true)
+	}	)	
+	const getArray = (i) => {
+		spn = spanLst[i].querySelectorAll('span')
+		img = slideLst[i].querySelectorAll('li')
+	}
+	const changeItem = (array, type, className = 'active') => {
+		for (i = 0; i < array.length; i++){
+			if(array[i].classList.contains(className)){
+				array[i].classList.remove(className)
+				switch(type){
+					case true:
+						i < array.length - 1 ? newItem = array[i + 1] :	newItem = array[0]
+					break;
+					default:
+						i > 0 ? newItem = array[i - 1] : newItem = array[array.length - 1]
+		}	}	}	newItem.classList.add(className)
+	}
