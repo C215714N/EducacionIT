2a5d5324372f7f6cfa3ba90cff5bc93b47a6d0c1
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-09 22:14:03 -0300

    Reglas generales

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,27 @@
 	mapFrm  = document.querySelector('#footer .cover')
 
 	mapBtn.onclick = () => mapFrm.classList.toggle('active') ? mapBtn.classList.replace('icon-max','icon-min') : mapBtn.classList.replace('icon-min', 'icon-max')
+/*Galeria de Imagenes*/
+	slide = document.querySelectorAll('.slide')
+	prev = document.querySelectorAll('.icon-prev')
+	next = document.querySelectorAll('.icon-next')
+
+	prev.forEach( (p,i) => p.onclick = () => changeItem(slide[i].querySelectorAll('li') ))
+	next.forEach( (n,i) => n.onclick = () => changeItem(slide[i].querySelectorAll('li'), true ))
+
+	const changeItem = (array, type, className = 'active') => {
+		for (i = 0; i < array.length; i++){
+			if(array[i].classList.contains(className)){
+				array[i].classList.remove(className)
+				switch(type){
+					case true:
+						i < array.length - 1 ?
+							newItem = array[i + 1] :
+							newItem = array[0]
+					break;
+					default:
+						i > 0 ?
+							newItem = array[i - 1] :
+							newItem = array[ array.length - 1 ]
+		}	}	}	newItem.classList.add(className)
+	}
