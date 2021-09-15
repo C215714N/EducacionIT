fe64aaff5a017557da95482b43daae45d2bdf0df
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-14 21:24:22 -0300

    Implementando LESS

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 /*Declarations*/
 	const d = document
+	const menuBtn = d.querySelector('nav .btn')
+	const menuLst = d.querySelector('nav .menu')
 	const pBar = d.querySelectorAll('.progress progress')
 	const pIn  = d.querySelectorAll('.progress input') 
 
+// Navigation Bar
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+
+// Progress Bar
 	pIn.forEach( (cmd,i) => cmd.addEventListener( 
 		'input', () => pBar[i].value = cmd.value 
 	)	)
