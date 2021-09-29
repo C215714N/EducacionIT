eede88cbc7e8bcaae57687a698742636cf3853c4
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-28 22:12:10 -0300

    Preview Funcion Galeria

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@
 	const menuLst = d.querySelector('nav .menu')
 	const pBar = d.querySelectorAll('.progress progress')
 	const pIn  = d.querySelectorAll('.progress input') 
+	const slideLst = d.querySelectorAll('.slide li')
+	const prev = d.querySelector('.icon-prev')
+	const next = d.querySelector('.icon-next')
 
 // Navigation Bar
 	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
@@ -12,5 +15,17 @@
 	pIn.forEach( (cmd,i) => cmd.addEventListener( 
 		'input', () => pBar[i].value = cmd.value 
 	)	)
-/*Functions*/
+/*Gallery*/
+	prev.onclick = () => changeItem(slideLst)
+	next.onclick = () => changeItem(slideLst)
 
+	const changeItem = (array, type, className = 'active') => {
+		for(i = 0; i < array.length; i++){
+			if(array[i].classList.contains(className)){
+				getDir('encontrado');
+			}
+		}
+	}
+	const getDir = (type) => {
+		alert(type);
+	}
