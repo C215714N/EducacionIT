23fd52ac0e58d5c44caefed506f92f988de34a6b
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-10-20 11:31:45 -0300

    Creacion DB

---
+++ DML_pylgs.sql
@@ -0,0 +1,8 @@
+/*  DATA DEFINITION LANGUAGE
+	lenguaje de definicion de datos
+    se utiliza para crear la estructura de la base de datos
+    modificarla e incluso eliminar la misma */
+## Creacion de DB
+DROP DATABASE IF EXISTS pepe_y_los_globos_store;
+CREATE DATABASE pepe_y_los_globos_store;
+USE pepe_y_los_globos_store;
