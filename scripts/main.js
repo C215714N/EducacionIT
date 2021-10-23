a95938bc260e8fe5867ba84677fbca0330e2125f
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-10-23 17:47:08 -0300

    Mapa de Contacto

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,10 @@
 	const d = document
 	const menuBtn = d.querySelector('nav .btn')
 	const menuLst = d.querySelector('nav .menu')
+	const mapBtn = d.querySelector('.map .btn')
+	const mapFrm = d.querySelector('.map iframe')
 /*Menu Hamburguesa*/
 	menuBtn.onclick = () => menuLst.classList.toggle('active')
+
+/*Mapa de Contacto*/
+	mapBtn.onclick = () => mapFrm.classList.toggle('active') ? mapBtn.classList.replace('icon-max','icon-min') : mapBtn.classList.replace('icon-min','icon-max')
