d87388536435b9226cf5b9962f5627aff1f61be6
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-11-04 21:35:36 -0300

    Galeria slide (unica)

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,10 @@
 	const pBar = d.querySelector('.progress progress')
 	const pIn = d.querySelector('.progress input')
 	const ctx = d.querySelector('#canvas').getContext('2d')
+	const prev = d.querySelector('.icon-prev')
+	const next = d.querySelector('.icon-next')
+	const slideLst = d.querySelectorAll('.slide li')
+	const slideSpn = d.querySelectorAll('.controls span')
 //navigation bar
 	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 //progress bar
@@ -27,3 +31,19 @@
 	/*Texto*/
 		ctx.font = "bold 1rem calibri, sans-serif"
 		ctx.strokeText("JAVASCRIPT CANVAS", 50, 250) /*text, x, y*/
+//gallery
+	prev.onclick = () => ( changeItem(slideLst), changeItem(slideSpn) )
+	next.onclick = () => ( changeItem(slideLst, true), changeItem(slideSpn, true) )
+
+	const changeItem = (array, type, className = 'active') => {
+		for(i = 0; i < array.length ; i++){
+			if(array[i].classList.contains(className)){
+				array[i].classList.remove(className)
+				switch(type){
+					case true:
+						i < array.length - 1 ? newItem = array[i + 1] : newItem = array[0]
+					break;
+					default:
+						i > 0 ? newItem = array[i - 1] : newItem = array[array.length - 1]
+		}	}	}	newItem.classList.add(className)
+	}
