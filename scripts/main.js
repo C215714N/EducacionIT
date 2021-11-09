b35b91152a5d93bfe5c5dfe583b19131a1d8498e
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-11-09 13:01:26 -0300

    Logica navegacion

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Constantes
+	const d = document
+	const menuBtn = d.querySelector('nav .btn')
+	const menuLst = d.querySelector('nav .menu')
+// Navegacion
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
