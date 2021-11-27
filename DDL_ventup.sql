bc1b890cabd806cc2e51ef6f2016400245d84e71
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-11-26 21:04:05 -0300

    Eliminacion y Creacion DB

---
+++ DDL_ventup.sql
@@ -0,0 +1,9 @@
+/* Data Definition Language
+	Crear/eliminar/editar 
+    las bases de datos,
+    las tablas y usuarios.
+*/
+-- creacion de DB
+DROP DATABASE IF EXISTS ventup;
+CREATE DATABASE ventup;
+USE ventup;
