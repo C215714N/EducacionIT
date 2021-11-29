0862a9b27aa858c5c31183f5dc22034191e60db7
Cristian Racedo <cristiandracedo@hotmail.com>
2021-11-29 10:37:19 -0300

    Galeria de Imagenes simple

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,30 @@
 	const pBar = d.querySelector('.progress progress')
 	const pIn = d.querySelector('.progress input')
 	const ctx = d.querySelector('#canvas').getContext('2d')
+	const prevBtn = d.querySelector('.icon-prev')
+	const nextBtn = d.querySelector('.icon-next')
+	const slideLst = d.querySelectorAll('#header .slide li')
+	const spanLst = d.querySelectorAll('#header .control span')
+/* Galeria de imagenes*/
+	const getItem = (array = [], dir = false, className = 'active') => {
+		for(i = 0; i < array.length; i++){
+			if(array[i].classList.contains(className)){
+				array[i].classList.remove(className)
+				switch(dir){
+					case true:
+						i < array.length - 1 ?
+							newItem = array[i + 1] :
+							newItem = array[0]
+					break;
+					default:
+						i > 0 ?
+							newItem = array[i - 1] :
+							newItem = array[array.length - 1]
+		}	}	}	newItem.classList.add(className)
+	}
+	prevBtn.onclick = () => ( getItem(slideLst), getItem(spanLst) )
+	nextBtn.onclick = () => ( getItem(slideLst, true), getItem(spanLst, true) )
+
 /* Progress Bar */
 	pIn.addEventListener('input',() => pBar.value = pIn.value)
 /* Canvas*/
