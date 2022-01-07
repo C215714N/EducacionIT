01765cca66392f285e5a31dd3f71627939466ce1
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-01-07 14:38:59 -0300

    Modificacion de tablas

--- DDL-proyecto_it.sql
+++ DDL-proyecto_it.sql
@@ -6,7 +6,12 @@
 	DROP DATABASE IF EXISTS proyecto_it; -- eliminacion (advertencia)
 	CREATE DATABASE proyecto_it; -- nombre
     USE proyecto_it; -- selecciono DB
-    
+/*
+	users => users_data
+    categories => products => posts
+    users => posts => sales
+    users => sales
+*/
 # Creacion de Tablas
 	CREATE TABLE users(
 		user_id INT AUTO_INCREMENT, -- numero entero
@@ -15,3 +20,12 @@
         PRIMARY KEY(user_id), -- clave principal (relacional)
         UNIQUE KEY(user_name) -- clave unica (local)
     );
+    ALTER TABLE users -- modifica una tabla existente
+    ADD COLUMN user_email VARCHAR(100) NOT NULL AFTER user_name, -- indico el orden
+    MODIFY COLUMN user_name VARCHAR(20) NOT NULL; -- almacena un dato de forma obligatoria
+    
+    DESCRIBE users;
+
+# Control
+	SHOW DATABASES;
+    SHOW TABLES;
