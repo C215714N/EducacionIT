ce28507387521ad775d6bd1a9eb206c86455344f
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-02-10 21:15:04 -0300

    Reproductor Video

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,15 @@
     const d = document
     const pBars = d.querySelectorAll('.progress progress')
     const pIns = d.querySelectorAll('.progress input[type="number"]')
+    const mVids = d.querySelectorAll('.media :is(audio, video)')
+    const mBtns = d.querySelectorAll('.media .btn-play')
 
 // Barra de progreso
     pIns.forEach((control,i) => control.addEventListener('input', () => pBars[i].value = control.value ))
+
+// Contenido Multimedia
+    mBtns.forEach((button, i) => button.onclick = () => {
+        let text
+        mVids[i].paused ? ( mVids[i].play(), text = 'PAUSE' ) : ( mVids[i].pause(), text = 'PLAY' )
+        button.innerHTML = text
+    })
