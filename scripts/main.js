a138c14188241f59c4da3ab248034995fdcbce74
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-02-21 10:39:11 -0300

    Mapa de contacto

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,19 @@
     const d = document
     const menuBtn = d.querySelector('#menu')
     const menuLst = d.querySelector('nav .menu')
-
+    const mapBtn = d.querySelector('#footer .map .btn')
+    const mapFrm = d.querySelector('#footer iframe')
 //  menu hamburguesa
     menuBtn.onclick = () => {
         let text
         // condicion ? valor V : valor F
         menuLst.classList.toggle('active') ? text = '&times;' : text = '&equiv;'
         menuBtn.innerHTML = text
+    }
+
+//  mapa de contacto
+    mapBtn.onclick = () => {
+        mapFrm.classList.toggle('active') ? 
+            mapBtn.classList.replace('icon-max','icon-min') : 
+            mapBtn.classList.replace('icon-min', 'icon-max')
     }
