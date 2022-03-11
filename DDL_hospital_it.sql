20b8f2399c4183d9701781f9d6e6eaee88ee0854
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-03-11 15:01:37 -0300

    DDL - Modificacion Tabla Pacientes

--- DDL_hospital_it.sql
+++ DDL_hospital_it.sql
@@ -18,14 +18,25 @@ USE hospital_it; -- seleccionamos la db
 */
 
 ## tabla pacientes
-CREATE TABLE pacients(
-	pacient_id INT AUTO_INCREMENT, -- nombre tipo_dato(numero) atributo
-    pacient_surname VARCHAR(50), -- cadena de texto variable
-    pacient_name VARCHAR(50), -- maximo 50 caracteres
+CREATE TABLE patients(
+	patient_id INT AUTO_INCREMENT, -- nombre tipo_dato(numero) atributo
+    patient_surname VARCHAR(50), -- cadena de texto variable
+    patient_name VARCHAR(50), -- maximo 50 caracteres
     birth_date DATE, -- campo de fecha 'YYYY-MM-DD'
     blood_group ENUM('0','A','B','AB'), -- lista de valores
     blood_type ENUM('+','-') DEFAULT '+', -- valor predeterminado
     cuit CHAR(13), -- cadena de texto fija '99-99999999-9'
     PRIMARY KEY(pacient_id) -- clave primaria (campo relacional)
 );
-DESCRIBE pacients;
+ALTER TABLE patients
+	CHANGE COLUMN patient_surname surname VARCHAR(50),
+	CHANGE COLUMN patient_name name VARCHAR(50),
+	ADD COLUMN address VARCHAR(200),
+	ADD COLUMN country VARCHAR(100);
+ALTER TABLE patients
+	ADD CONSTRAINT UNIQUE KEY(cuit),
+    ADD CONSTRAINT UNIQUE KEY(surname, name, birth_date, address, country);
+/* cambios de nombre en la tabla
+	RENAME TABLE pacients TO patients;
+*/
+DESCRIBE patients;
