a4f358d1c61ca1cfb28f33896480a090bdfc4e68
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-05-03 13:08:38 -0300

    Script de Envio de Correo

--- scripts/contact.php
+++ scripts/contact.php
@@ -7,20 +7,27 @@
 //  Creacion del mensaje    
     $message = "
         <strong>Nombre: </strong> $name <br>
-        <strong>Correo: </strong> $email <br>
         <strong>Consulta: </strong> $consult
     ";
+    mail(
+        "info@misitio.com", // destinatario
+        "Consulta Web",     // asunto
+        $message,           // cuerpo
+        $email              // remitente
+    );
+
+//  Redireccionamiento
     echo("
         <link rel=stylesheet href=../styles/main.css>
         <header id=header>
             <h1>Fundamentos Web</h1>
             <p>html | css | js</p>
         </header>
-        <p>
+        <p class=message>
             muchas gracias $name por enviar tu consulta,
             estaremos contactandonos con vos a la brevedad
             al correo $email. Podes seguir visitando nuestra pagina desde el siguiente <a href=../forum.html>link</a>
         </p>
-    ")
-//  Redireccionamiento
+    ");
+    header("location: ../forum.html");
 ?>
