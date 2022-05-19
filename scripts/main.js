708b31169c6927acdce64f2fc1e5d1f9cfb6c6c1
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-05-19 17:20:05 -0300

    Logica Menu Hamburguesa

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,22 @@
 // Declaraciones
     const d = document
+    const menuList = d.querySelector('#header .menu');
+    const menuButton = d.querySelector('#header .btn');
     const mapFrame = d.querySelector('#footer .map iframe');
     const mapButton = d.querySelector('#footer .map .btn');
+    let cName = 'active';
 
 // Eventos
-
+/* Menu Hamburguesa */
+    menuButton.addEventListener('click', () => 
+        menuList.classList.toggle(cName) ?
+            menuButton.innerHTML = '&times;' :
+            menuButton.innerHTML = '&equiv;'
+    )
 /* Mapa de Contacto */
     mapButton.onclick = () => {
         // Operador Ternario condicion ? valor V : valor F
-        mapFrame.classList.toggle('active') ? 
+        mapFrame.classList.toggle(cName) ? 
         // codicion (se agrego la clase active)
             mapButton.classList.replace('icon-max','icon-min') : 
             // si se cumple (se reemplaza icon-max por icon-min)
