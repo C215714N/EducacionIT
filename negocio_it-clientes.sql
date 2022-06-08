e4bbbf76612b11317dc198b7c205662375197a28
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-06-08 20:57:12 -0300

    DML Consulta de Clientes

--- negocio_it-clientes.sql
+++ negocio_it-clientes.sql
@@ -1,4 +1,4 @@
-/* DML (Data Manipulation Language)
+/* DDL (Data Definition Language)
 	Crear/Modificar/Eliminar Estructuras
     Base de datos / tablas / usuarios
 */
@@ -28,3 +28,28 @@ CREATE TABLE customers (
     UNIQUE KEY(cuit) -- campo local (unico)
 );
 DESCRIBE customers;
+
+/* DML (Data Manipulation Language)
+	Crear/Modificar/Eliminar Registros
+*/
+
+# Carga Completa de Datos (ordinal)
+INSERT INTO customers -- tabla
+VALUES (
+	0, 				-- customer_id
+    'Racedo', 		-- last_name
+    'Cristian', 	-- first_name
+    '20-35336446-5' -- cuit
+);
+# Carga selectiva de Datos (opcional)
+INSERT INTO customers(cuit, last_name)
+VALUES ('27-34983422-3','Prince');
+
+# Carga Multiple de Datos (selectiva)
+INSERT INTO customers(last_name,first_name,cuit)
+VALUES  ('Kent','Clark','23-28327389-4'),
+		('Howlett','James','21-12938472-5'),
+		('Parker','Peter','20-46912848-5'),
+        ('Kyle','Selina','26-47382913-8');
+# Consulta de datos
+SELECT * FROM customers;
