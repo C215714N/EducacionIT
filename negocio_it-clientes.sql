eab89943275e7b3c2806d87c5524b2d79649bc76
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-06-22 22:08:13 -0300

    DML - Actualizacion Correos y Calculo de Edades

--- negocio_it-clientes.sql
+++ negocio_it-clientes.sql
@@ -1,6 +1,6 @@
 /* DDL (Data Definition Language)
 	Crear/Modificar/Eliminar Estructuras
-    Base de datos / tablas / usuarios
+    Base de datos / tablAS / usuarios
 */
 
 # Creacion Base de datos
@@ -9,12 +9,12 @@ CREATE DATABASE negocio_it;
 USE negocio_it; -- seleccionamos la base de datos
 
 /* Planificacion
-	clientes => ventas => cliente / producto
+	clientes => ventAS => cliente / producto
     
     categoria   => 	productos => ventas/compras
-    marca		=>  productos => ventas/compras 
+    marca		=>  productos => ventas/comprAS 
     
-    proveedores => compras => proveedor / producto
+    proveedores => comprAS => proveedor / producto
 */
 
 # Tabla Clientes
@@ -27,6 +27,19 @@ CREATE TABLE customers (
     PRIMARY KEY(customer_id), -- campo relacional (unico)
     UNIQUE KEY(cuit) -- campo local (unico)
 );
+-- modificacion de la estructura
+ALTER TABLE customers
+	-- nuevAS columnas
+	ADD COLUMN birth_date DATE,
+	ADD COLUMN description VARCHAR(50) AFTER cuit,
+	ADD COLUMN address VARCHAR(100) NOT NULL,
+    ADD COLUMN email VARCHAR(100) NOT NULL,
+    -- nueva restriccion
+    ADD CONSTRAINT UNIQUE KEY(last_name, first_name, birth_date, address, email);
+-- modificacion de campos existentes
+ALTER TABLE customers
+	MODIFY COLUMN birth_date DATE NOT NULL, -- cambiamos tipo de dato
+    CHANGE COLUMN description notes VARCHAR(200); -- cambiamos el nombre y tipo de dato
 DESCRIBE customers;
 
 /* DML (Data Manipulation Language)
@@ -41,11 +54,11 @@ VALUES (
     'Cristian', 	-- first_name
     '20-35336446-5' -- cuit
 );
-# Carga selectiva de Datos (opcional)
+# Carga SELECTiva de Datos (opcional)
 INSERT INTO customers(cuit, last_name)
 VALUES ('27-34983422-3','Prince');
 
-# Carga Multiple de Datos (selectiva)
+# Carga Multiple de Datos (SELECTiva)
 INSERT INTO customers(last_name,first_name,cuit)
 VALUES  ('Kent','Clark','23-28327389-4'),
 		('Howlett','James','21-12938472-5'),
@@ -53,6 +66,8 @@ VALUES  ('Kent','Clark','23-28327389-4'),
         ('Kyle','Selina','26-47382913-8');
 # Consulta de datos
 SELECT * FROM customers;
+
+# Consulta por iniciales P.P
 SELECT last_name, first_name FROM customers
 WHERE last_name LIKE 'p%' OR first_name LIKE 'p%';
 
@@ -60,6 +75,29 @@ WHERE last_name LIKE 'p%' OR first_name LIKE 'p%';
 UPDATE customers -- tabla a manipulacion
 SET first_name = 'Diana' -- valor de actualizacion
 WHERE customer_id = 2; -- registro unico
+
+# Funcion de agrupar (Segun casos)
+UPDATE customers 
+SET email = 
+	CASE
+		WHEN customer_id IN(2,4,7) THEN CONCAT(first_name,'_', last_name, '@dc.net')
+		WHEN customer_id BETWEEN 3 AND 8 THEN CONCAT(first_name,'_', last_name, '@marvel.com')
+		ELSE CONCAT(first_name, last_name, '@gmail.com')
+	END
+WHERE email IS NULL OR email = '';
+
+## extraccion de valores
+SELECT
+	LEFT(cuit,2) AS identify_id,
+    RIGHT(cuit,1) AS validator_digit,
+    SUBSTRING(cuit,4,8) AS document
+FROM customers;
+
+## calcular edades
+SELECT CONCAT_WS(' ',first_name,last_name,email) AS customer_data, -- concatenacion de caracteres
+	DATEDIFF(CURRENT_DATE(),birth_date) / 365 AS edad, -- diferencia en dias, convertido a años
+    YEAR(CURRENT_DATE()) - YEAR(birth_date) AS age -- diferencia de edades en años
+FROM customers;
 
 # verificar bases de datos y tablas
 SHOW DATABASES;
