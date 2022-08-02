2354c4c803c9ab85528fa642f05e9c9404faf38d
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-08-01 22:20:37 -0300

    DML - Insercion de Registros en tabla Usuarios

--- escuela_it.sql
+++ escuela_it.sql
@@ -26,3 +26,34 @@ CREATE TABLE users(
     UNIQUE KEY(username)
 );
 DESCRIBE users;
+## Tabla Datos de Usuario
+CREATE TABLE users_data(
+	data_id INT AUTO_INCREMENT,
+    user INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    birth_date DATE,
+    cuil INT,
+    address VARCHAR(200),
+    phone VARCHAR(20),
+    PRIMARY KEY(data_id),
+    UNIQUE KEY(cuil),
+    UNIQUE KEY(first_name, last_name, birth_date, address, phone),
+    FOREIGN KEY(user) REFERENCES users(user_id)
+);
+DESCRIBE users_data;
+
+/*	DML - DATA MANIPULATION LANGUAGE
+	INSERTAR / ACTUALIZAR / ELIMINAR / CONSULTAR => registros
+*/
+
+# Tabla Usuarios
+## Carga completa (ordinal)
+INSERT INTO users
+VALUES (0,'c215714n','cristiandracedo@hotmail.com','root');
+## Carga Selectiva
+INSERT INTO users(email, username) 
+VALUES ('cristiandracedo@gmail.com','Cristian');
+
+## Consulta de Datos
+SELECT * FROM users;
