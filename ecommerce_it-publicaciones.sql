30c422ba73d68e2a30276f091408337766e98f1d
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-08-17 21:14:43 -0300

    DML - Publicacion y Venta de Productos

--- ecommerce_it-publicaciones.sql
+++ ecommerce_it-publicaciones.sql
@@ -38,3 +38,30 @@ ADD CONSTRAINT FOREIGN KEY(user) REFERENCES users(user_id); -- agregamos clave f
 
 ## Carga de Publicaciones
 SELECT * FROM products;
+INSERT INTO posts(user, product, post_title, stock, price)
+VALUES 	(1,6,'Memoria HyperX 16gb ddr4',20,24500), -- Memoria RAM | c215714n
+		(4,9,'Zapatillas Talles del 21 al 50', 5000, 31999), -- Adadis MAX | Batman
+        (1,10,'Tarjeta RX3070 Gddr6 10Gb',50,285000), -- Placa de Video | c215714n
+        (1,2,'Tab A13"', 200, 65300), -- Tablet | c215714n
+		(3,7,'Calzado Air MAX Talles del 39 al 70',2500,45800); -- Air Max  | cristian
+
+## Carga de Ventas
+### Carga por consulta SELECT campo unico
+INSERT INTO sales(user) -- tabla a insertar registros
+SELECT user_id FROM users WHERE user_id > 4; -- lista de valores a cargar
+## Carga por consulta SELECT multiples campos
+INSERT INTO sales(post, quantity, price) -- tabla objetivo
+SELECT post_id, CEIL(stock / 100), price FROM posts; -- consulta de origen
+
+## Actualizacion aleatoria
+### Numero de publicacion
+UPDATE sales 
+SET post = CEIL(RAND() * 5) -- numero aleatorio entre 1 y 5 (redondeado hacia arriba)
+WHERE post IS NULL; -- siempre que post no este especificado en la venta (NULO)
+
+### Numero de Usuario
+UPDATE sales
+SET user = FLOOR(RAND() * 5 + 3) -- usuario entre 3 y 7 (redondeado hacia abajo)
+WHERE user IS NULL; -- siempre que el usuario no este definido (NULL)
+
+SELECT * FROM sales;
