ef43c280441e27bf96fa7c61047e44eb5e8c96e5
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-08-25 18:14:41 -0300

    Consulta Condicional

--- scripts/condition.js
+++ scripts/condition.js
@@ -2,54 +2,48 @@ let d = document;
 let w = window;
 
 let cuentas = d.querySelector('#cuentas');
+a = w.confirm('¿deseas realizar una cuenta?')
+/** Condiconal Logico
+ *  Evalua una condicion no estricta
+ *  Ejecuta el codigo si el resultado es verdadero
+ *  caso contrario, continua con la ejecucion
+ */
+if(a){
 
-let num_a = parseInt(w.prompt('escriba un numero'));
-let num_b = parseInt(w.prompt('escriba otro numero'));
-let operation = w.prompt('elija una operacion:\n 1-suma\n 2-resta\n 3-multiplicacion\n 4-division');
-// condicional logico
-/*
-    if (operation == 1){
-        alert(num_a + num_b)
-    } else if (operation == 2){
-        alert(num_a - num_b);
-    } else if (operation == 3){
-        alert(num_a * num_b);
-    } else if (operation == 4){
-        if(num_b == 0){
-            alert('No se puede dividir entre 0');
-        } else {
-            alert(num_a / num_b);
-        }
-    } else {
-        alert('La opcion seleccionada NO es valida');
-    }
+    let num_a = parseInt(w.prompt('escriba un numero'));
+    let num_b = parseInt(w.prompt('escriba otro numero'));
+    let operation = w.prompt('elija una operacion:\n 1-suma\n 2-resta\n 3-multiplicacion\n 4-division');
+
+/** Condicional por casos 
+ *  Evalua una condicion de manera estricta
+ *  Compara multiples valores para una misma condicion
+ *  Ejecuta el codigo y continua a menos que se interrumpa  
 */
-
-// Condicional por casos
-switch(operation){
-    case '1':
-    case 'suma':
-    case 'adicion':
-        alert(num_a + num_b);
-    break;
-    case '2':
-    case 'resta':
-    case 'sustraccion':
-        alert(num_a - num_b);
-    break;
-    case '3':
-    case 'producto':
-    case 'multiplicacion':
-        alert(num_a * num_b);
-    break;
-    case '4':
-    case 'division':
-        if(num_b == 0){
-            alert('No se puede dividir entre 0');
-        } else {
-            alert(num_a / num_b);
-        }
-    break;
-    default: 
-        alert('selecciona una opcion valida');
+    switch(operation){
+        case '1':
+        case 'suma':
+        case 'adicion':
+            alert(num_a + num_b);
+        break;
+        case '2':
+        case 'resta':
+        case 'sustraccion':
+            alert(num_a - num_b);
+        break;
+        case '3':
+        case 'producto':
+        case 'multiplicacion':
+            alert(num_a * num_b);
+        break;
+        case '4':
+        case 'division':
+            if(num_b == 0){
+                alert('No se puede dividir entre 0');
+            } else {
+                alert(num_a / num_b);
+            }
+        break;
+        default: 
+            alert('selecciona una opcion valida');
+    }
 }
