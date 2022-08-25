3f967cd1b8ed63aa03068180f2863f7849c90a59
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-08-25 16:08:33 -0300

    Bucles Do While

--- scripts/loops.js
+++ scripts/loops.js
@@ -15,10 +15,18 @@ while(count < limit){
     count++ // incremento en 1
     whileParagraph.innerHTML += count + ' '; // asignacion y concatenacion al parrafo
 }
-
+// Array (Arreglo): Lista de elementos que pueden pertenecer a diferentes tipos de datos. Cada elemento posee un indice
+let doList = d.querySelector('#do');
+let seasons = ['verano','otoño','invierno','primavera'];
+let n = 0;
 /**
  *  DO WHILE: 
  *  Repite el bloque de codigo mientras se cumpla la condicion 
  *  se debe utilizar un factor de modificacion (a++) para evitar un bucle infinito
  *  Las instrucciones se ejecutan al menos una vez
 */
+
+do {
+    doList.innerHTML += '<li>' + seasons[n] + '</li>';
+    n++
+} while(n < seasons.length)
