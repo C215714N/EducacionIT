5a9d4389e2e60fe88d896fb6ff1c9485121e869d
Cristian Racedo <cristiandracedo@hotmail.com>
2022-08-31 01:28:08 -0300

    Revisiones calculadora

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,35 +1,38 @@
+// Declaraciones
 let d = document;
+// Elementos del DOM
 let result = d.querySelector('#result');
 let action = d.querySelector('#actions');
 let numbers = d.querySelector('#numbers');
 let operators = d.querySelector('#operators');
+// Valores de referencia
 let simbols = ['=','+','-','x','/'];
 let letters = ['CE','C','DEL'];
 let account = [];
 let check = false;
 
-function clearAll (){
+// Limpieza de Resultado
+function clearAll(){
     result.value = '';
-    account = [];
 }
-
+// Renderizacion de Elementos
 function renderElements (tag, array = [], limit = 10) {
     for (i = 0; i < limit; i++){
         tag.innerHTML += `<input type="button" value="${ array[i] ? array[i] : i}">`
 }   }
-
+// Asignacion de Funciones
 function renderAction(tag){
     let buttons = tag.querySelectorAll('input');
-    buttons.forEach( btn => btn.onclick = () => callAction(btn.value)
-)   }
-
+    buttons.forEach( btn => btn.onclick = () => callAction(btn.value) )   
+}
+// Carga de Valor Numerico
 function addNumber (x){
     if (account[3] && result.value) {
         clearAll()
     }
-    result.value = parseInt(result.value + x);
+    return result.value = parseInt(result.value + x);
 }
-
+// Almacenamiento del Numero
 function setNumber (){
     if (check == false){ 
         account[0] = result.value;
@@ -37,7 +40,7 @@ function setNumber (){
         account[2] = result.value;
     }
 }
-
+// Asignacion de Operacion
 function callOperation(x){
     if ( x != '=') {
         x == 'x' ? account[1] = '*' : account[1] = x;
@@ -48,7 +51,7 @@ function callOperation(x){
         account[3] = result.value;
         check = false;
 }   }
-
+// Funcion derivadora
 function callAction(x){
     if ( x >= 0 && x <= 9 ){
         addNumber(x);
@@ -64,10 +67,11 @@ function callAction(x){
         callOperation(x);
     }
 }
-
+// Renderizacion
 renderElements(numbers);
 renderElements(operators, simbols, simbols.length);
 renderElements(actions, letters, letters.length);
+// Asignacion
 renderAction(actions);
 renderAction(numbers); 
 renderAction(operators);
