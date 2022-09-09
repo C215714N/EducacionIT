a32e97ffd985e84caeeb2d8eb69deda4f71f8eed
Cristian Racedo <cristiandracedo@hotmail.com>
2022-09-08 21:51:32 -0300

    Logica Mapa de contacto

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const d = document;
+const mapButton = d.querySelector('#footer .map');
+const mapFrame = d.querySelector('#footer .cover');
+
+
+
+mapButton.onclick = () => alterClass(mapButton, mapFrame, 'icon-max', 'icon-min');
+
+function alterClass(el, tag, a, b){
+    tag.classList.toggle('active') ?
+    el.classList.replace(a,b) :
+    el.classList.replace(b,a)
+}
