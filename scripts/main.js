f01af5281537281e0f5022fb7af6585f927dbf6e
Cristian Racedo <cristiandracedo@hotmail.com>
2022-09-13 20:58:37 -0300

    Estructura Tabla de contenidos

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,16 @@ function alterClass(el, tag, a, b){
     el.classList.replace(a,b) :
     el.classList.replace(b,a)
 }
+
+// fotos = [
+//     'bart',
+//     'lisa',
+//     'maggie',
+//     'marge'
+// ]
+// const tag = d.querySelector('.css-grid.dynamic.masonry');
+
+// for(foto of fotos){
+//     tag.innerHTML += `<img src="assets/gallery/${foto}.png" alt="foto de ${foto}">
+//     `
+// }
