29ed12337c39331c57cf263c2ab2f0af8a79dda0
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-14 21:50:44 -0300

    Estilos Componente Boton y Barra de Navegacion

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,14 @@
 // Importaciones
+const d = document;
+const menuBtn = d.querySelector('nav .btn');
+const menuList = d.querySelector('nav .menu');
 
+menuBtn.addEventListener('click', (e) => {
+    if ( menuList.classList.toggle('active') ) {
+        e.target.classList.replace('menu-open', 'menu-close') 
+    } else {
+        e.target.classList.replace('menu-close', 'menu-open')
+    }
+})
 
 // Autor: Cristian Racedo
