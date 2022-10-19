040149c7fa0d7960ea198ea28ca3bc27e89ee355
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-19 12:11:55 -0300

    Formulario de contacto: Campo de Asunto

--- scripts/contact.php
+++ scripts/contact.php
@@ -2,15 +2,20 @@
 // Captura de Datos
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $subject = $_POST['subject'];
     $consult = $_POST['consult'];
 
 // Creacion del Mensaje
     mail(
         "info@misitio.com", // Destinatario
-        "consulta web", // Asunto
+        $subject, // Asunto
         $consult, // Mensaje
         "From: $name<$email>"  // Remitente  
     );
 // Redireccionamiento
-    header("Location: ../about.html");
+    // header("Location: ../about.html");
 ?>
+<h1>Fundamentos web</h1>
+<p>
+    Muchas gracias por realizar su consulta, nos pondremos en contacto a la brevedad. Puede volver a la pagina principal haciendo click en el siguiente <a href="../about.html">link</a>
+</p>
