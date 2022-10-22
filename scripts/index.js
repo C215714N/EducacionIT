283659633ae385b9d0cab7aee3116cd3cfc54b6a
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-21 22:16:12 -0300

    Galeria Slide

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@
 const d = document;
 const menuBtn = d.querySelector('nav .btn');
 const menuList = d.querySelector('nav .menu');
+const prev = d.querySelector('.control.icon-prev');
+const next = d.querySelector('.control.icon-next');
+const gallery = d.querySelectorAll('.slide li');
 
 menuBtn.addEventListener('click', (e) => {
     if ( menuList.classList.toggle('active') ) {
@@ -11,4 +14,22 @@ menuBtn.addEventListener('click', (e) => {
     }
 })
 
+prev.addEventListener('click', () => getItem('prev'))
+next.addEventListener('click', () => getItem('next'))
+
+function getItem(type) {
+    let nextElement;
+    for(let img of gallery){
+        if(img.classList.contains('active')){
+            img.classList.remove('active');
+            if (type == 'next'){
+                nextElement = img.nextElementSibling || img.parentNode.firstElementChild
+            }
+            else if (type == 'prev'){
+                nextElement = img.previousElementSibling || img.parentNode.lastElementChild
+            }
+        }
+    }
+    nextElement.classList.add('active')
+}
 // Autor: Cristian Racedo
