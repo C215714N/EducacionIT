a459c4727763c6219cddf708f922ee5e6e5521ba
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-11-29 12:08:08 -0300

    Logica Funcion alternar

--- scripts/events.js
+++ scripts/events.js
@@ -1,7 +1,27 @@
 const
     d = document,
+    className = 'active',
     menuBtn = d.getElementById('menu'),
-    menuList = d.querySelector('nav .menu')
+    menuList = d.querySelector('nav .menu'),
+    mapBtn = d.getElementById('map'),
+    mapFrm = d.querySelector('#footer > .map')
 
 // Evento click
-menuBtn.onclick = () => menuList.classList.toggle('active');
+menuBtn.onclick = (e) => toggle({
+    btn: e.target, 
+    target: menuList,
+    val: ['menu-close', 'menu-open']
+} )
+mapBtn.onclick = (e) => toggle( {
+    val: ['map-close', 'map-open'],
+    target: mapFrm, 
+    btn: e.target
+} )
+
+// Funcion toggle
+function toggle(obj) {
+    obj.target.classList.toggle(className) ?
+    obj.btn.classList.replace(obj.val[1],obj.val[0]) :
+    obj.btn.classList.replace(obj.val[0],obj.val[1])
+}
+
