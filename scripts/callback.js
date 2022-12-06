be8c8fcc505c54820765d08ee867d22749564384
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-06 13:50:34 -0300

    eventos y funciones autoinvocadas

--- scripts/callback.js
+++ scripts/callback.js
@@ -1,8 +1,11 @@
 // Declaraciones: Ver en events.js
-let images = ['osx-mavericks','osx-monterrey','osx-mojave']
+let images = ['osx-mavericks','osx-lion','osx-mojave']
 const 
     btnGallery = d.querySelector('#btnGallery'),
-    slides = d.getElementsByClassName('carrousel')
+    slides = d.getElementsByClassName('carrousel'),
+    prev = d.querySelectorAll('.carrousel .prev'),
+    next = d.querySelectorAll('.carrousel .next')
+
 // Funciones y metodos
 function addGallery(){
     let container = d.getElementById('gallery');
@@ -33,4 +36,32 @@ function addEvents(buttons, items){
 btnGallery.addEventListener('click', addGallery);
 getCarrousel(slides, addEvents)
 
-// implementacion
+/* GALERIA CARROUSEL */
+const searchElementInArray = function (array) {
+    for(item of array){
+        if(item.classList.contains('active')){
+            item.classList.remove('active');
+            console.log(item);
+            return item;
+} } }
+
+const setActiveElement = (array, buttonType) => {
+    let lastActiveItem = searchElementInArray(array);
+    if(buttonType == 'prev'){
+        lastActiveItem = lastActiveItem.previousElementSibling || lastActiveItem.parentNode.lastElementChild;
+    } else if (buttonType == 'next'){
+        lastActiveItem = lastActiveItem.nextElementSibling || lastActiveItem.parentNode.firstElementChild;
+    } else {
+        lastActiveItem = array[buttonType];
+    }
+    lastActiveItem.classList.add('active');
+}
+
+prev.forEach( (btn, i) => { btn.addEventListener('click', () => {
+    let imageList = slides[i].querySelectorAll('.slide .item');
+    setActiveElement(imageList,'prev');
+} ) } )
+next.forEach( (btn, i) => { btn.addEventListener('click', () => {
+    let imageList = slides[i].querySelectorAll('.slide .item');
+    setActiveElement(imageList,'next');
+} ) } )
