5bb6d7c46155e312528080575acf46a3c40c72ca
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-10 16:29:51 -0300

    Funcion para alternar clases

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,13 @@ const
     mapBtn = d.querySelector('#footer [class*="map-"]');
     mapFrm = d.querySelector('#footer .map'),
 
-mapBtn.addEventListener('click', () => 
-    mapFrm.classList.toggle('active') ?
-    mapBtn.classList.replace('map-open','map-close') :
-    mapBtn.classList.replace('map-close', 'map-open')
-);
+mapBtn.addEventListener('click', (e) => toggle(
+    e.target, mapFrm, 'map-open', 'map-close'
+) );
+
+/* Funciones */
+function toggle(btn, target, a, b){
+    target.classList.toggle('active') ?
+    btn.classList.replace(a, b) :
+    btn.classList.replace(b, a)
+}
