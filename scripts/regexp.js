4f9ff9c0b65ee1364837854b4a29a0f5cf26e576
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-15 00:00:53 -0300

    Validacion RegExp.test()

--- scripts/regexp.js
+++ scripts/regexp.js
@@ -25,19 +25,40 @@ const formControls = d.querySelectorAll('#contact input');
 
 formControls.forEach( input => {
     input.addEventListener('input', (e) => {
-        console.log(e.target.checkValidity())
-    } )
+    console.log(
+        `checkValidity(): ${e.target.checkValidity()}`, 
+        `RegExp.test(): ${validation(e.target.value, e.target.type)}`)
+    })
 });
 
-let texto = /(\+*)[a-zA-Z]+/
-let numero = new RegExp('[0-5]$','g')
 
-console.log('string simple ' + texto.test('texto'))
-console.log('numero ' + texto.test(14))
-console.log('string compuesto ' + texto.test('esto es otro texto con mas de 20 caracteres'))
-
-function isRegularNum(arg){
-    numero.test(arg) ?
-    (console.log('felicitaciones, pasaste la prueba'), console.log('ok')) :
-    console.log('no es un numero valido para esta prueba');
+function validation(args, type){
+    let regexp;
+    switch(type){
+        case 'text':
+            regexp = /[a-z A-Z]+/;
+            // solamente letras (1 o mas repeticiones)
+        break;
+        case 'number':
+            regexp = new RegExp(/^\d+$/);
+            // solamente digitos (1 o mas repeticiones)
+        break;
+        case 'password':
+            regexp = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+            // mayusculas minusculas numeros y simbolos (8 caracteres o mas)
+        break;
+        case 'date':
+            regexp = new RegExp(/\d{1,2}\/\d{1,2}\/\d{2,4}/);
+            // fomato de fecha d/m/yy | dd/mm/yyyy
+        break;
+        case 'tel':
+            regexp = /\+*\(\d{2,5}\)(\d{2,4}\-){1,4}\d{4}/;
+            // formato de telefono +(99)9999-9999 | (99999)99-99-99-99-9999
+        break;
+        case 'email':
+            regexp = new RegExp(/\w{2,}@\w{2,}(\.[a-zA-Z]{2,})+/);
+            // formato de correo usuario@dominio
+        break;
+    }
+    return regexp.test(args)
 }
