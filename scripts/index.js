1cce0c11f641ab2da87fb8c5514d503ec138d464
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-17 15:11:35 -0300

    Logica Navegacion

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
+/* Declaraciones */
 const
     d = document,
     w = window,
+    // navigation
+    menuBtn = d.querySelector('nav .btn'),
+    menuLst = d.querySelector('nav .menu'),
     // contact map
     mapBtn = d.querySelector('#footer [class*="map-"]');
     mapFrm = d.querySelector('#footer .map'),
+
+/* Eventos */
+menuBtn.addEventListener('click', (e) => toggle(
+    e.target, menuLst, 'menu-open', 'menu-close'
+) );
 
 mapBtn.addEventListener('click', (e) => toggle(
     e.target, mapFrm, 'map-open', 'map-close'
