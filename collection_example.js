a9b609c9b1f12340b2bfe77b5ebaa4758d787be4
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-23 22:07:44 -0300

    Ejemplo coleccion de datos

---
+++ collection_example.js
@@ -0,0 +1,17 @@
+[
+    {
+        id: 1,
+        name: 'cristian',
+        active: true
+    },
+    {
+        id: 2,
+        name: 'damian',
+        active: false
+    },
+    {
+        id: 3,
+        name: 'racedo',
+        active: true
+    },
+]
