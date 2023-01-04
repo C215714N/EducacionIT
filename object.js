f914a356d906a362cf2beda0b7f77d6a0ed399ef
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-01-04 18:52:58 -0300

    Expresion de Objeto con coleccion

---
+++ object.js
@@ -0,0 +1,16 @@
+const object = {
+    name: 'cristian',
+    lastName: 'racedo',
+    age: 31,
+    instructor: true,
+    courses: [
+        {
+            name: 'poo',
+            days: 'lu mi vi 14hs'
+        },
+        {
+            name: 'html',
+            days: 'lu mi vi 14hs'
+        }
+    ]
+};
