19583b904f27ec62c00827ebd61a0a9d837891a8
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-01-05 11:04:31 -0300

    Modularizacion e importacion de scripts

--- scripts/app/index.js
+++ scripts/app/index.js
@@ -1,3 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('la pagina cargo exitosamente');
+import {d, server} from "./global.js";
+import fetchData from "./fetchData.js";
+import usersTable from "./users/usersTable.js";
+import usersPosts from "./posts/usersPosts.js";
+
+d.addEventListener('DOMContentLoaded', () => {
+/** API de Eventos
+ *  Metodos
+ *  preventDefault(): evita el comportamiento por defecto (generalmente refresh)
+ *  stopPropagation(): evita la fase de captura de eventos
+ */
+d.addEventListener('click', async(e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    // Acciones para los hipervinculos
+    if (e.target.tagName == 'A'){
+        e.target.href.includes('mailto') ? window.open() : root.innerHTML = '';
+        // Solicitudes al servidor BACKEND
+        if(e.target.attributes['fetch-type'].value == 'server'){
+            const page = e.target.href.split('#')[1];
+            const results = await fetchData({url: `${server}/${page}`})
+            // Elemento a renderizar por enlace
+            page == 'users' ?
+                usersTable(results) :
+            page == 'posts' ?
+                usersPosts(results) :
+                console.log(results)
+        }
+    }
+} )
 } )
