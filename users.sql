296283427041806a11b03afe0c6277068248113d
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-01-14 11:08:50 -0300

    Actualizacion tabla usuarios y carga de registros

--- users.sql
+++ users.sql
@@ -26,6 +26,12 @@ CREATE TABLE users(
     PRIMARY KEY(id), -- clave principal (relacional)
     UNIQUE KEY(username) -- campo unico (local)
 );
+# modificacion de estructura
+ALTER TABLE users
+MODIFY employee BOOLEAN DEFAULT false, -- modificamos el campo
+ADD COLUMN created DATETIME DEFAULT current_timestamp; -- agregamos un campo (al final)
+# descripcion de la estructura
+DESCRIBE users;
 
 /* DML: Data Manipulation Language
 	insertar / eliminar / actualizar / consultar
@@ -36,6 +42,18 @@ INSERT INTO users(username, userpass)
 VALUES ('cristian', 'root');
 
 INSERT INTO users(username, employee, userpass)
-VALUES('c215714n',true,'12345');
+VALUES ('c215714n', true, 'r00t');
 
+# Carga Multiple
+INSERT INTO users(username, userpass)
+VALUES	('Maria', 'pass'),
+		('R4m1r0','user'),
+        ('Damian','access'),
+        ('c215714n!#','vtodyosm'),
+        ('admin','d48w58qj');
+
+# consulta general de datos
 SELECT * FROM users;
+
+# reinicio de tabla (eliminar registros)
+TRUNCATE users; -- vuelve el contador a 0
