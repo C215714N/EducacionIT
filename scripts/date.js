ece91cd382d764edfba92b2510446b3684b9a340
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-01-17 13:18:29 -0300

    Clase Reloj para temporizador

--- scripts/date.js
+++ scripts/date.js
@@ -14,3 +14,47 @@ let clock = setInterval(
     () => copy.innerHTML = timer(), 
     1000
 );
+
+class Clock {
+    constructor(h, m, s){
+        this.hr = h;
+        this.min = m,
+        this.sec = s;
+    }
+    adjustTime(value){
+        return value < 10 ? '0'+ value : value
+    }
+    getTime(){
+        let h = this.adjustTime(this.hr)
+        let m = this.adjustTime(this.min)
+        let s = this.adjustTime(this.sec)
+        return `${h}:${m}:${s}`
+    }
+    getHours(){
+        return this.hr
+    }
+    getMinutes(){
+        return this.min;
+    }
+    getSecond(){
+        return this.sec;
+    }
+    setTime(key, value){
+        (value >= 60 && key != 'hr') ||
+        (value >= 24 && key == 'hr') ?
+        this[key] = 0 :
+        this[key] = value;
+    }
+}
+
+let Cron = new Clock(0,0,0)
+let runTimer = setInterval(()=>{
+    Cron.setTime('sec', Cron.getSecond() + 1)
+    
+    if (Cron.getSecond() == 0) 
+        Cron.setTime('min', Cron.getMinutes() + 1)
+    if (Cron.getMinutes() == 0 && Cron.getSecond() == 0) 
+        Cron.setTime('hr', Cron.getHours('hr') + 1)
+
+    console.log(Cron)
+}, 1)
