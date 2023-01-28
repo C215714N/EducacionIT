7ea8990a4894c5b5743d7bc94b5e9ad5f909a991
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-01-28 11:00:07 -0300

    Tabla datos de usuarios y relativas

---
+++ docs/users_data.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM users
+LIMIT 0, 1000
+
+-- Date: 2023-01-28 10:24
+*/
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (1,'cristian','root',0,'2023-01-28 10:15:12');
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (2,'c215714n','r00t',1,'2023-01-28 10:15:12');
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (3,'Maria','pass',1,'2023-01-28 10:15:12');
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (4,'R4m1r0','user',0,'2023-01-28 10:15:12');
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (5,'Damian','access',0,'2023-01-28 10:15:12');
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (6,'c215714n!#','vtodyosm',1,'2023-01-28 10:15:12');
+INSERT INTO `` (`id`,`username`,`userpass`,`employee`,`created`) VALUES (7,'admin','d48w58qj',1,'2023-01-28 10:15:12');
