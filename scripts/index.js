09d127a3ba20afef9d2af9ca3c999ef62fc5eeb8
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-02-10 20:16:33 -0300

    logica: evento click organizado por accion y clases

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,55 @@
 // Declaraciones
 const 
     d = document,
+    className = 'active',
     // Navegacion
     nav = d.getElementById('nav'),
     menuButton = d.querySelector('#nav .btn'),
-    menuList = d.querySelector('#nav .menu')
+    menuList = d.querySelector('#nav .menu'),
+    // Mapa de contacto
+    mapFrame = d.querySelector('iframe.map')
 ;
 
 // Funciones
-
+function toggle(object){
+    const { btn, target, list, className } = object;
+    // ternario: condicion ? valor V : valor F
+    target.classList.toggle(className) ?
+    btn.classList.replace(list[0], list[1]) :
+    btn.classList.replace(list[1], list[0])
+}
 
 // Eventos
 d.onclick = (e) => {
-    if(
-        e.target.tagName === 'BUTTON' ||
-        e.target.tagName === 'A' 
-    ){
-    menuList.classList.toggle('active')
-} }
+    let btn = e.target, target, list;
+    const action = btn.attributes['action'].value;
+    const type = btn.classList;
+
+    if(action == 'toggle'){
+        if (type.contains('nav-toggle')){
+            btn = menuButton;
+            target = menuList;
+            list = ['icon-open', 'icon-close'];
+        }
+        if (type.contains('map')){
+            target = mapFrame;
+            list = ['icon-plus', 'icon-less'];
+        }
+        toggle({
+            btn,
+            target,
+            className,
+            list,
+        })
+    }
+    else if(action == 'select'){
+        console.log(e)
+    }
+    else{
+        console.log('no hay action');
+    }
+}
 
 nav.onmouseleave = () => setTimeout( 
-    () => menuList.classList.remove('active'), 5000 
+    () => menuList.classList.remove('active'), 15000 
 )
