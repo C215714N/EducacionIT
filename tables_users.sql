4f9fcce9cba662c2041788c973b0bc1a27dd3e2c
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-13 16:15:40 -0300

    Creacion de tabla usuarios

--- tables_users.sql
+++ tables_users.sql
@@ -7,3 +7,16 @@
 DROP DATABASE IF EXISTS escuelait; -- eliminacion condicional
 CREATE DATABASE escuelait; -- creacion
 USE escuelait; -- seleccion
+
+# Creacion Tabla Usuarios
+CREATE TABLE users(
+	id INT AUTO_INCREMENT, -- numero entero (4.294.967.296)
+    username VARCHAR(20), -- texto variable
+    userpass VARCHAR(20), -- entre 0 y 20
+    # Restricciones
+    PRIMARY KEY(id), -- campo relacional(clave principal - unico)
+    UNIQUE KEY(username) -- campo unico (local)
+);
+
+# detalle de tabla
+DESCRIBE users;
