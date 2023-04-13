6efc463ef882e88f1f683ed23127569a72db77b0
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-13 15:58:10 -0300

    Creacion y seleccion de base de datos

---
+++ tables_users.sql
@@ -0,0 +1,9 @@
+/* 	DDL (Data Definition Language) 
+	Crear Base de datos / Tablas
+    Estructura con la que vamos a trabajar
+*/
+
+# Crear La Base de datos
+DROP DATABASE IF EXISTS escuelait; -- eliminacion condicional
+CREATE DATABASE escuelait; -- creacion
+USE escuelait; -- seleccion
