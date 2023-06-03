59d7c9d55ba55432f040d50686431b3f9fe7c039
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-06-02 22:16:09 -0300

    script de contacto

---
+++ scripts/contact.php
@@ -0,0 +1,18 @@
+<?php
+// Captura de Datos
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$consult = $_POST['consult'];
+
+// Generacion del Mensaje
+mail(
+    'info@misitio.com', // Destinatario
+    $subject,           // Asunto
+    $consult,           // Cuerpo
+    "From:$name<$email>"// Remitente
+);
+
+// Redireccionamiento
+header("location: ../about.html");
+?>
