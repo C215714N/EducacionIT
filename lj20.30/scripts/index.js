c2cee42f6c2bbf07c73cf04cb1d43134f8e48935
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-06-29 21:45:58 -0300

    logica mapa de contacto

--- lj20.30/scripts/index.js
+++ lj20.30/scripts/index.js
@@ -1,21 +1,31 @@
-// Declaraciones
+/* Declaraciones */
 const 
     d = document,
     className = 'active',
     // Navegacion
     menuBtn     = d.querySelector('#nav .btn'),
-    menuList    = d.querySelector('#nav .menu')
+    menuList    = d.querySelector('#nav .menu'),
+    // Mapa de Contacto
+    mapBtn      = d.querySelector('#footer button[class*="map-"]'),
+    mapFrame    = d.querySelector('#footer iframe.map')
 ;
 
-// Funciones
+/* Funciones */
 function toggle({target, btn, val = []}){
     target.classList.toggle(className) ?
     btn.classList.replace( val[0], val[1] ) :
     btn.classList.replace( val[1], val[0] )
 }
-// Eventos
+/* Eventos */
+// Navegacion
 menuBtn.addEventListener('click', (e) => toggle({
     target: menuList,
     btn: e.target,
     val: ['menu-open', 'menu-close']
+} ) )
+// Mapa de Contacto
+mapBtn.addEventListener('click', (e) => toggle({
+    target: mapFrame,
+    btn: e.target,
+    val: ['map-open','map-close']
 } ) )
