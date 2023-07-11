70eb6023e24df26367a9649946c9fe8722b1329d
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-07-10 22:05:08 -0300

    logica galeria de imagenes con transicion

--- lj20.30/scripts/index.js
+++ lj20.30/scripts/index.js
@@ -7,14 +7,38 @@ const
     menuList    = d.querySelector('#nav .menu'),
     // Mapa de Contacto
     mapBtn      = d.querySelector('#footer button[class*="map-"]'),
-    mapFrame    = d.querySelector('#footer iframe.map')
+    mapFrame    = d.querySelector('#footer iframe.map'),
+    // Galeria de imagenes
+    slider      = d.querySelectorAll('.slider .item'),
+    prev        = d.querySelector('.slider .icon-prev'),
+    next        = d.querySelector('.slider .icon-next'),
+    controls    = d.querySelectorAll('.slider .controls button')
 ;
 
 /* Funciones */
+// Alternar clases de un elemento y reemplazar clases en otro
 function toggle({target, btn, val = []}){
     target.classList.toggle(className) ?
     btn.classList.replace( val[0], val[1] ) :
     btn.classList.replace( val[1], val[0] )
+}
+// Capturar un elemento dentro de un array
+function getItem({array = [], className}){
+for (let element of array){
+    if (element.classList.contains(className)){
+        element.classList.remove(className)
+        return element
+}   }   }
+// Definir una clases a una elemento x
+function setItem({array = [], type = "NEXT", className}){
+    let item = getItem({array, className});
+    item = (
+    type == "NEXT" ? 
+    (item.nextElementSibling || item.parentNode.firstElementChild) :
+    type == "PREV" ? 
+    (item.previousElementSibling || item.parentNode.lastElementChild) : 
+    array[type]) 
+    item.classList.add(className)
 }
 /* Eventos */
 // Navegacion
@@ -29,3 +53,18 @@ mapBtn.addEventListener('click', (e) => toggle({
     btn: e.target,
     val: ['map-open','map-close']
 } ) )
+// Galeria de Imagenes
+next.addEventListener('click', () => {
+    setItem({array: slider, className })
+    setItem({array: controls, className })
+} )
+prev.addEventListener('click', () => {
+    setItem({array: slider, type: "PREV", className })
+    setItem({array: controls, type: "PREV", className })
+} )
+controls.forEach((btn, i) => btn.addEventListener('click', () => {
+    setItem({array: slider, type: i, className})
+    setItem({array: controls, type: i, className})
+} ) )
+// Transicion automatica
+window.setInterval(() => next.click(), 60000)
