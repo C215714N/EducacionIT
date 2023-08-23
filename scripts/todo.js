cb36aa3055a9aa55f286112110b95c20c267a9ad
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-08-23 17:13:19 -0300

    lista de tareas con JS

---
+++ scripts/todo.js
@@ -0,0 +1,55 @@
+const 
+    d = document,
+    tareas = [
+        'aprender HTML en 2 simples pasos',
+        'dominar CSS y no morir en el intento',
+        'Crear Hojas de estilos increiblemente largas con SASS',
+        'Aprender Javascript y no dormirme en el intento',
+        'Morfosis amigos'
+    ],
+    div = d.querySelector('.container'),
+    todoInput = d.getElementById('todoInput'),
+    todoList = d.getElementById('todoList'),
+    addButton = d.getElementById('addButton'),
+    delFirstButton = d.createElement('button'),
+    delLastButton = d.createElement('button');
+/* Metodos de Array
+    array.push() -> agrega un elemento al final del array
+    array.unshift() -> agrega un elemento al inicio del array
+    array.pop() -> elimina el ultimo elemento del array
+    array.shift() -> elimina el primer elemento del array
+*/
+
+delFirstButton.innerText= 'Eliminar Primero';
+delLastButton.innerText= 'Eliminar Ultimo';
+
+delFirstButton.setAttribute('class','btn btn-warning');
+delLastButton.setAttribute('class','btn btn-danger');
+
+div.append(delFirstButton, delLastButton);
+
+// Funciones y Eventos
+addButton.onclick = function () {
+    if (todoInput.value.length >= 4){
+        tareas.unshift(todoInput.value);
+        printList(tareas);
+        todoInput.value = ''
+    }
+}
+delFirstButton.onclick = function() {
+    tareas.shift();
+    printList(tareas);
+}
+delLastButton.onclick = function() {
+    tareas.pop();
+    printList(tareas);
+}
+// Carga la lista dentro de UL
+function printList(array){
+    todoList.innerHTML = '';
+    for(let item of array){
+        todoList.innerHTML += '<li class="list-group-item">' + item + '</li>'
+    }
+}
+
+printList(tareas);
