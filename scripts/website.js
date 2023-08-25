3875499cab3420978e2503a85f45cf41a76ebd18
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-08-25 15:37:49 -0300

    funcion de creacion de contenido

--- scripts/website.js
+++ scripts/website.js
@@ -12,7 +12,7 @@ const
     root = d.getElementById('root'),
     // Pie de Pagina
     social = d.querySelector('#footer .social'),
-    copy = d.querySelector('#footer .copy'),
+    copy = d.querySelector('#footer .icon-copy'),
 /* Declaraciones Globales  */
     // Galeria
     path = "assets/bg/",
@@ -44,5 +44,40 @@ const
     today = new Date();
 
 /* Funciones  */
+function createGallery(array, element){
+    for (let item of array){
+        let a = '';
+        // EL primer elemento tiene la clase active
+        if (array.indexOf(item) === 0) a ='active';
+        // Agrega el elemento a la lista
+        element.innerHTML += 
+        '<li class="gallery-item '+a+'">'+
+            '<img src='+path+item+' alt="Imagen de Galeria">'+
+        '</li>' 
+} }
+function addButtons(n, element){
+    for(let i = 0; i < n; i++){
+        let button = d.createElement('button');
+        button.onclick = function(){
+            console.log('soy el boton n' + i)
+        }
+        element.append(button)
+    }
+}
+function addLinks(array, element){
+    for (let item of array){
+        if (typeof item === 'string'){
+            element.innerHTML += '<a class='+item+' href="https://'+item+'.com" target="_blank"></a>';
+        }
+        else {
+            element.innerHTML +='<li>'+'<a href='+item['link']+'>'+item['text']+'</a>'+'</li>'
+        }
+    }
+}
 
+createGallery(images, galleryList);
+addButtons(images.length, controls);
+addLinks(links, navigation);
+addLinks(socialLinks, social);
+copy.prepend(today.getFullYear()+' ');
 /* Eventos */
