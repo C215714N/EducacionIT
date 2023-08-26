17555e897ea8a6ab7cd83f01c9582ec165e5d275
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-08-25 22:05:58 -0300

    consultas de actualizacion

--- tablas_capacitaciones.sql
+++ tablas_capacitaciones.sql
@@ -71,15 +71,14 @@ SET user = (SELECT user_id FROM users WHERE username = "Cristian"), -- subconsul
 # Asignacion de Cursos por Profesor
 INSERT INTO courses_assignment(teacher, course, day)
 VALUES
--- Cristian
-	( teacher,
-     (SELECT id FROM courses WHERE name ), -- MySQL
-     "M,V"
-    ),
--- Messi
-	(
-    
-    );
+-- Cursos asignados a Cristian
+	( (SELECT id FROM teachers WHERE user = (SELECT user_id FROM users WHERE username = "cristian") ), -- subconsulta anidada
+	( SELECT id FROM courses WHERE name LIKE "%SQL%" ),	"M,V"), -- MySQL
+    ( 1, (SELECT id FROM courses WHERE name LIKE "%HTM%"), "M,J"),-- HTML5
+-- Cursos asignados a Messi
+	( (SELECT id FROM teachers WHERE user = (SELECT user_id FROM users WHERE username = "Messi") ), -- subconsulta anidada
+	( SELECT id FROM courses WHERE name LIKE "FIS%"),"X,V"), -- FISICA
+    ( 2, (SELECT id FROM courses WHERE name LIKE "BIO%"), "L,J"); -- BIOLOGIA
 
 ## Consulta de Profesores
 SELECT * FROM teachers;
@@ -97,4 +96,50 @@ SELECT * FROM courses WHERE name LIKE "%SQL%"; -- % cualquier cantidad al princi
 ## Cursos cuya segunda letra sea I
 SELECT * FROM courses WHERE name LIKE "_I%"; -- _ Un caracter cualquiera al inicio
 ## Cursos cuya penultima letra sea C
-SELECT * FROM courses WHERE name LIKE "%C_" -- _ Un caracter cualquier al final
+SELECT * FROM courses WHERE name LIKE "%C_"; -- _ Un caracter cualquier al final
+
+# Consulta de Cursos Asignados
+SELECT * FROM courses_assignment;
+
+## Actualizacion de Horarios Segun el dia
+UPDATE courses_assignment -- Tabla a actualizar
+SET start_time = -- actualizacion condicional
+	CASE -- inicio de la actualizacion por casos
+		WHEN day LIKE "%V%" THEN "10:00" -- si contiene V entonces: 10hs
+		WHEN day LIKE "%L%" THEN "14:00" -- si NO contiene V y contiene L entonces: 14hs
+		ELSE "20:00" -- Si no contiene V ni L entonces para todos los demas casos: 20hs
+	END, -- fin del analisis por casos
+	end_time = ADDTIME(start_time, "02:00") -- el valor de la hora + 2HS
+WHERE start_time IS NULL; -- Actualiza el registro si el horario de inicio no existe
+
+## Actualizacion de Fechas de Inicio y fin para Cristian
+UPDATE courses_assignment
+SET start_date = "2023-07-18", -- asignacion de valor
+	end_date =  -- actualizacion condicional
+    CASE -- inicio de actualizacion por casos 
+		WHEN day LIKE "%V%" THEN "2023-09-01" -- si contiene V entonces: 1/9/23
+        ELSE "2023-08-22" -- si no es V, para los demas casos: 22/8/23
+    END -- fin de la actualizacion por casos
+WHERE teacher = 1 AND start_date IS NULL; -- profesor Cristian y que no posea fecha de inicio
+
+## Actualizacion de Fechas para Messi (por descarte)
+UPDATE courses_assignment
+SET start_date = -- Actualizacion condicional
+	CASE
+		WHEN day LIKE "%X%" THEN "2023-07-12"
+        ELSE "2023-07-27"
+	END,
+    end_date = ADDDATE(start_date, INTERVAL 28 DAY) -- agrega un intervalo de 28 a la fecha de inicio
+WHERE start_date IS NULL; -- solamente actualiza si NO EXISTE la fecha de inicio
+
+## Consulta calculada
+SELECT 
+	id, 
+    teacher, 
+    course, 
+    DATEDIFF(end_date, start_date) AS days, -- ALIAS (apodo para referirnos al campo calculado)
+    TIMEDIFF(end_time, start_time) AS time, -- ALIAS (campo que NO EXISTE en la tabla original)
+    day
+FROM courses_assignment
+WHERE start_time >= "10:00" AND start_time <= "18:00" -- entre las 10hs y las 18hs
+HAVING days >= 30; -- Condicion para Campos Calculados (ALIAS)
