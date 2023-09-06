1e0a3c104d6606658d432115a52aa9f60ce11b62
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-09-05 21:03:19 -0300

    usuarios de conexion al servidor de base de datos

---
+++ usuarios_servidordb.sql
@@ -0,0 +1,38 @@
+/* DCL - Data Control Language 
+	crear, eliminar y asignar permisos
+    usuarios
+*/
+## Usuario Administrador
+DROP USER IF EXISTS 'admin'@'localhost'; -- eliminamos si existe
+CREATE USER 'admin'@'localhost' -- creacion del usuario en servidor
+IDENTIFIED BY 'root'; -- le asignamos la contraseña
+## Permisos (Control Total sobre EscuelaIT)
+GRANT ALL PRIVILEGES -- instrucciones a ejecutar
+ON escuelait.* -- database.tables (* => todas las tablas)
+TO 'admin'@'localhost'; -- usuario designado
+
+## Usuario Instructor (Manipulacion de datos)
+DROP USER IF EXISTS 'teacher'@'localhost'; -- eliminacion si existe
+CREATE USER 'teacher'@'localhost' -- usuario en servidor
+IDENTIFIED BY 'pass'; -- contraseña
+## Permisos (Control y Edicion de Registros)
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON escuelait.*
+TO 'teacher'@'localhost';
+
+## Usuario Alumno (Carga y Consulta de Datos)
+DROP USER IF EXISTS 'student'@'localhost'; -- eliminamos si existe
+CREATE USER 'student'@'localhost' -- usuario en servidor
+IDENTIFIED BY '1234'; -- contraseña
+GRANT SELECT, INSERT
+ON escuelait.*
+TO 'student'@'localhost';
+
+## Usuario Invitado (Lectura de datos)
+DROP USER IF EXISTS 'guest'@'localhost'; -- eliminamos si existe
+CREATE USER 'guest'@'localhost' -- usuario en servidor
+IDENTIFIED BY '000'; -- contraseña
+## Permisos (Solo lectura - Version de prueba)
+GRANT SELECT
+ON escuelait.*
+TO 'guest'@'localhost';
