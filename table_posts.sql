9bfe8d117758e437c27a8409b622ee2635ca0450
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-12 13:11:10 -0300

    consultas por agrupacion

--- table_posts.sql
+++ table_posts.sql
@@ -27,6 +27,13 @@ CREATE TABLE sales(
 );
 
 /* DML - Data Manipulation Language */
+## Eliminacion de datos (DESACTIVAR safe updates)
+DELETE FROM posts; -- tabla a eliminar registros
+ALTER TABLE posts AUTO_INCREMENT = 1; -- reinicio de numeracion
+
+## Reinicio de tabla (No deben existir relaciones con esta tabla)
+TRUNCATE sales; -- elimina los registros y reestablece la numeracion
+
 # Tabla Publicaciones de usuarios
 ## Carga Multiple de Publicaciones
 INSERT INTO posts(title, user, product, price, stock)
@@ -40,9 +47,39 @@ VALUES
     ("Acondicionador Pega la Vuelta", 5, 45,3900,5400); -- Galan Joaquin
 
 # Tabla Ventas por publicacion
-
+INSERT INTO sales(post, user, quantity)
+VALUES 
+	(4,1,2),(6,1,3),(7,1,2),(1,1,1), -- Racedo Cristian
+    (3,6,5),(2,6,1),(4,6,10); -- Fernandez Leandro
 
 # Consultas
 ## Publicaciones del mes de Septiembre
 SELECT * FROM posts
 WHERE post_date BETWEEN '2023-09-01' AND '2023-09-30'; -- rango de numeros
+## TOTALES por Venta Realizada
+SELECT user, post, price, quantity,
+	price * quantity AS total -- campo calculado (no existe)
+FROM sales;
+
+## Cantidad de Ventas por Publicacion
+SELECT 
+	post,
+	COUNT(post) sales, -- cantidad de ventas
+    SUM(quantity) AS products, -- total de productos vendidos
+    ROUND(AVG(quantity)) AS average, -- promedio de productos vendidos
+    SUM(price * quantity) AS total -- total por venta
+FROM sales
+GROUP BY post; -- agrupado por publicacion
+
+## Gastos por usuario
+SELECT
+	user,
+    COUNT(user) AS bought, -- cuenta los registros
+    SUM(price * quantity) AS total -- suma de totales 
+FROM sales
+GROUP BY user; -- agrupado por usuario
+
+# Actualizacion de Precios en ventas
+UPDATE sales
+SET price = (SELECT price FROM posts WHERE id = post)
+WHERE price IS NULL;
