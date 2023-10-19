403810cea710372d4288f26cf132c2758f7c8087
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-19 12:30:04 -0300

    asignacion de permisos usuarios de conexion

---
+++ control_users.sql
@@ -0,0 +1,28 @@
+/* DCL - DATA CONTROL LANGUAGE */
+
+## Usuario Administrador
+DROP USER IF EXISTS 'admin'@'localhost'; -- Eliminamos el usuario si existe
+CREATE USER 'admin'@'localhost' -- usuario en servidor
+IDENTIFIED BY 'R00t'; -- credenciales (contraseña)
+### Permisos
+GRANT ALL PRIVILEGES -- control Total
+ON smsapp.* -- sobre todas las tablas de la DB
+TO 'admin'@'localhost'; -- para el usuario
+
+## Usuario Estandar
+DROP USER IF EXISTS 'user'@'localhost'; -- Eliminamos si existe
+CREATE USER 'user'@'localhost' -- usuario en servidor
+IDENTIFIED BY 'p455'; -- credenciales (contraseña)
+### Permisos
+GRANT SELECT, INSERT, UPDATE -- lectura, escritura, actualizacion
+ON smsapp.* -- sobre todas las tablas
+TO 'admin'@'localhost'; -- para el usuario
+
+## Usuario Invitado
+DROP USER IF EXISTS 'guest'@'localhost'; -- Eliminamos el usuario si existe
+CREATE USER 'guest'@'localhost' -- usuario en servidor
+IDENTIFIED BY '1234'; -- credenciales (contraseña)
+### Permisos
+GRANT SELECT -- control Total
+ON smsapp.* -- sobre todas las tablas de la DB
+TO 'admin'@'localhost'; -- para el usuario
