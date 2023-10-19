46886f057fc8ba491cda797657904194796e575d
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-19 12:08:46 -0300

    actualizacion de backups en distintos formatos

--- backups/smsapp.sql
+++ backups/smsapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-10-2023 a las 15:38:50
+-- Tiempo de generación: 19-10-2023 a las 16:50:22
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -78,13 +78,22 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `description`, `user`, `product`, `post_date`, `price`, `stock`) VALUES
-(1, 'Mesa Ratona de Coleccion', '', 4, 22, '2023-10-17 10:28:53', 50000.0000, 6),
-(2, 'Sommier Queen Size', '', 4, 24, '2023-10-17 10:28:53', 125999.9900, 2),
-(3, 'Tablet Galaxy Tab10', '', 5, 21, '2023-10-17 10:28:53', 155320.0000, 10),
-(4, 'Zapatillas Pimpi Edicion Limitada', '', 4, 4, '2023-10-17 10:28:53', 35780.0000, 2000),
-(5, 'Aire Split Inverter 3000 Frigorias', '', 6, 27, '2023-10-17 10:28:53', 359999.9900, 10),
-(6, 'Smart Tv 59 Pulgadas', '', 6, 26, '2023-10-17 10:28:53', 236800.0000, 50),
-(7, 'Acondicionador Pega la Vuelta', '', 5, 45, '2023-10-17 10:28:53', 3900.0000, 5400);
+(1, 'Mesa Ratona de Coleccion', '', 4, 22, '2023-09-12 12:30:54', 60000.0000, 1),
+(2, 'Sommier Queen Size', '', 4, 24, '2023-10-01 12:30:54', 151199.9880, 5),
+(3, 'Tablet Galaxy Tab10', '', 5, 21, '2023-09-22 12:30:54', 186384.0000, 7),
+(4, 'Zapatillas Pimpi Edicion Limitada', '', 4, 4, '2023-07-15 12:30:54', 42936.0000, 12),
+(5, 'Aire Split Inverter 3000 Frigorias', '', 6, 27, '2023-10-12 12:30:54', 431999.9880, 6),
+(6, 'Smart Tv 59 Pulgadas', '', 6, 26, '2023-08-17 12:30:54', 284160.0000, 12),
+(7, 'Acondicionador Pega la Vuelta', '', 5, 45, '2023-09-10 12:30:54', 4680.0000, 35),
+(8, 'aire acondicionado publicado por joaquin', '', 5, 27, '2023-10-19 10:29:35', 281021.0952, 2),
+(9, 'mesa publicado por lucia', '', 4, 20, '2023-10-19 10:29:44', 66043.4335, 11),
+(10, 'sommier publicado por joaquin', '', 5, 24, '2023-10-19 10:29:45', 359062.7244, 5),
+(11, 'corbata publicada por lucia', '', 4, 7, '2023-10-19 10:29:45', 578.2830, 2000),
+(12, 'flores publicada por lucia', '', 4, 8, '2023-10-19 10:29:46', 4358.1193, 5400),
+(13, 'notebook publicado por joaquin', '', 5, 1, '2023-10-19 10:29:46', 435880.2648, 9),
+(14, 'netbook publicado por joaquin', '', 5, 2, '2023-10-19 10:29:47', 159417.8700, 8),
+(15, 'mesa de luz publicado por joaquin', '', 5, 19, '2023-10-19 10:29:47', 36601.7353, 9),
+(17, 'sommier publicado por lucia', '', 4, 24, '2023-10-19 10:29:48', 53956.9099, 8);
 
 -- --------------------------------------------------------
 
@@ -185,7 +194,7 @@ INSERT INTO `product_categories` (`id`, `product`, `category`) VALUES
 (1, 21, 2),
 (40, 21, 4),
 (25, 22, 8),
-(38, 23, 2),
+(38, 23, 8),
 (39, 24, 4),
 (9, 25, 4),
 (41, 26, 4),
@@ -226,13 +235,44 @@ CREATE TABLE `sales` (
 --
 
 INSERT INTO `sales` (`id`, `post`, `price`, `quantity`, `user`, `sale_date`) VALUES
-(1, 4, 35780.0000, 2, 1, '2023-10-17 10:28:53'),
-(2, 6, 236800.0000, 3, 1, '2023-10-17 10:28:53'),
-(3, 7, 3900.0000, 2, 1, '2023-10-17 10:28:53'),
-(4, 1, 50000.0000, 1, 1, '2023-10-17 10:28:53'),
-(5, 3, 155320.0000, 5, 6, '2023-10-17 10:28:53'),
-(6, 2, 125999.9900, 1, 6, '2023-10-17 10:28:53'),
-(7, 4, 35780.0000, 10, 6, '2023-10-17 10:28:53');
+(1, 4, 35780.0000, 2, 1, '2023-10-12 12:44:49'),
+(2, 6, 236800.0000, 3, 1, '2023-10-12 12:44:49'),
+(3, 7, 3900.0000, 2, 1, '2023-10-12 12:44:49'),
+(4, 1, 50000.0000, 1, 1, '2023-10-12 12:44:49'),
+(5, 3, 155320.0000, 5, 6, '2023-10-12 12:44:49'),
+(6, 2, 125999.9900, 1, 6, '2023-10-12 12:44:49'),
+(7, 4, 35780.0000, 10, 6, '2023-10-12 12:44:49'),
+(8, 7, 3900.0000, 10, 17, '2023-10-19 10:52:46'),
+(9, 7, 3900.0000, 13, 11, '2023-10-19 10:52:47'),
+(10, 6, 236800.0000, 2, 16, '2023-10-19 10:52:48'),
+(11, 15, 30501.4461, 3, 17, '2023-10-19 10:52:49'),
+(12, 6, 236800.0000, 2, 15, '2023-10-19 10:52:50'),
+(13, 14, 132848.2250, 2, 15, '2023-10-19 10:52:51'),
+(14, 2, 125999.9900, 2, 12, '2023-10-19 10:52:52'),
+(15, 8, 234184.2460, 1, 10, '2023-10-19 10:52:52'),
+(16, 13, 363233.5540, 2, 17, '2023-10-19 10:52:53'),
+(17, 8, 234184.2460, 1, 13, '2023-10-19 10:52:54'),
+(18, 2, 125999.9900, 2, 9, '2023-10-19 10:52:55'),
+(19, 17, 44964.0916, 8, 15, '2023-10-19 10:52:55'),
+(20, 13, 363233.5540, 2, 14, '2023-10-19 10:52:56'),
+(21, 10, 299218.9370, 2, 9, '2023-10-19 10:52:56'),
+(22, 9, 55036.1946, 6, 12, '2023-10-19 10:52:57'),
+(23, 7, 3900.0000, 10, 15, '2023-10-19 10:52:58'),
+(24, 6, 236800.0000, 1, 14, '2023-10-19 10:52:58'),
+(25, 3, 155320.0000, 2, 15, '2023-10-19 10:52:59'),
+(26, 6, 236800.0000, 2, 12, '2023-10-19 10:52:59'),
+(27, 14, 132848.2250, 2, 16, '2023-10-19 10:52:59'),
+(28, 15, 30501.4461, 6, 16, '2023-10-19 10:53:00'),
+(29, 13, 363233.5540, 2, 10, '2023-10-19 10:53:00'),
+(30, 10, 299218.9370, 1, 10, '2023-10-19 10:53:01'),
+(31, 5, 431999.9880, 3, 16, '2023-10-19 10:53:02'),
+(32, 10, 359062.7244, 2, 10, '2023-10-19 10:53:02'),
+(33, 6, 284160.0000, 2, 17, '2023-10-19 10:53:03'),
+(34, 14, 159417.8700, 2, 10, '2023-10-19 10:53:03'),
+(35, 9, 66043.4335, 5, 16, '2023-10-19 10:53:03'),
+(36, 13, 435880.2648, 3, 10, '2023-10-19 10:53:04'),
+(37, 14, 159417.8700, 2, 11, '2023-10-19 10:53:07'),
+(38, 5, 431999.9880, 3, 12, '2023-10-19 10:53:07');
 
 -- --------------------------------------------------------
 
@@ -256,9 +296,19 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `active`) VALUES
 (1, 'c215714n', NULL, 'r00t', 1),
 (2, 'Cristian', NULL, '1234', 1),
 (4, 'lucia', NULL, 'p1mp1', 1),
-(5, 'joaquin', NULL, 'n3l4', 1),
-(6, 'leandro', NULL, 'alber38', 1),
-(7, 'joaquin1', NULL, 'g4l4n', 1);
+(5, 'joaquin', NULL, 'n3l4', NULL),
+(6, 'leandro', NULL, 'alber38', NULL),
+(7, 'joaquin1', NULL, 'g4l4n', NULL),
+(8, 'Bret', 'Sincere@april.biz', NULL, 1),
+(9, 'Antonette', 'Shanna@melissa.tv', NULL, 1),
+(10, 'Samantha', 'Nathan@yesenia.net', NULL, 1),
+(11, 'Karianne', 'Julianne.OConner@kory.org', NULL, 1),
+(12, 'Kamren', 'Lucio_Hettinger@annie.ca', NULL, 1),
+(13, 'Leopoldo_Corkery', 'Karley_Dach@jasper.info', NULL, 1),
+(14, 'Elwyn.Skiles', 'Telly.Hoeger@billy.biz', NULL, 1),
+(15, 'Maxime_Nienow', 'Sherwood@rosamond.me', NULL, 1),
+(16, 'Delphine', 'Chaim_McDermott@dana.io', NULL, 1),
+(17, 'Moriah.Stanton', 'Rey.Padberg@karina.biz', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -288,7 +338,17 @@ INSERT INTO `users_data` (`id`, `user`, `lastname`, `firstname`, `birthdate`, `g
 (1, 1, 'Racedo', 'Cristian', '1991-04-18', 'M', NULL, 'DNI', '35336446', NULL, NULL),
 (2, 6, 'Fernandez', 'Leandro', '2008-10-05', 'M', NULL, 'CUIL', '20-12983294-2', NULL, NULL),
 (3, 4, 'Galan', 'Lucia', '1961-05-23', 'F', NULL, 'CUIL', '27-08369065-3', NULL, NULL),
-(4, 5, 'Galan', 'Joaquin', '1953-07-21', 'M', NULL, 'CUIL', '20-10809452-5', NULL, NULL);
+(4, 5, 'Galan', 'Joaquin', '1953-07-21', 'M', NULL, 'CUIL', '20-10809452-5', NULL, NULL),
+(5, 8, 'Leanne Graham', 'Bret', NULL, NULL, NULL, NULL, '92998-3874', 'Kulas Light', '1-770-736-8031 x5644'),
+(6, 9, 'Ervin Howell', 'Antonette', NULL, NULL, NULL, NULL, '90566-7771', 'Victor Plains', '010-692-6593 x09125'),
+(7, 10, 'Clementine Bauch', 'Samantha', NULL, NULL, NULL, NULL, '59590-4157', 'Douglas Extension', '1-463-123-4447'),
+(8, 11, 'Patricia Lebsack', 'Karianne', NULL, NULL, NULL, NULL, '53919-4257', 'Hoeger Mall', '493-170-9623 x156'),
+(9, 12, 'Chelsey Dietrich', 'Kamren', NULL, NULL, NULL, NULL, '33263', 'Skiles Walks', '(254)954-1289'),
+(10, 13, 'Mrs. Dennis Schulist', 'Leopoldo_Corkery', NULL, NULL, NULL, NULL, '23505-1337', 'Norberto Crossing', '1-477-935-8478 x6430'),
+(11, 14, 'Kurtis Weissnat', 'Elwyn.Skiles', NULL, NULL, NULL, NULL, '58804-1099', 'Rex Trail', '2.100.676.132'),
+(12, 15, 'Nicholas Runolfsdottir V', 'Maxime_Nienow', NULL, NULL, NULL, NULL, '45169', 'Ellsworth Summit', '586.493.6943 x140'),
+(13, 16, 'Glenna Reichert', 'Delphine', NULL, NULL, NULL, NULL, '76495-3109', 'Dayna Park', '(775)976-6794 x41206'),
+(14, 17, 'Clementina DuBuque', 'Moriah.Stanton', NULL, NULL, NULL, NULL, '31428-2261', 'Kattie Turnpike', '024-648-3804');
 
 --
 -- Índices para tablas volcadas
@@ -362,7 +422,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT de la tabla `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
@@ -374,25 +434,25 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `product_categories`
 --
 ALTER TABLE `product_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de la tabla `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `users_data`
 --
 ALTER TABLE `users_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
