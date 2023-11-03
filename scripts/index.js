c4b0e85e4e39b4d5cdf0d5ccb3d8a5151f4f56fd
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-03 12:16:58 -0300

    script de validacion

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+// Declaraciones
+const formControls = document.querySelectorAll('.form-control');
+
+// Funciones
+const changes = ({classes = [], target}) => {
+    target.classList.remove(classes[0]);
+    target.classList.add(classes[1]);
+}
+
+const handleValidation = (target, a, b) =>
+    target.checkValidity() ?
+    changes({classes:[a,b], target}):
+    changes({classes:[b,a], target});
+
+// Eventos
+formControls.forEach(control => {
+    control.addEventListener('input', (e) => handleValidation(e.target, "is-invalid", "is-valid"))
+} )
