89e6d595d30caaa5c0d1d59ef7c12c696ee4604e
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-04-12 11:26:37 -0300

    comentarios de codigo

--- scripts/if.js
+++ scripts/if.js
@@ -11,18 +11,23 @@ if (pregunta){
     
     // Algoritmo, Procesamiento o logica interna
     if (Op == 1 || Op.toUpperCase() == "SUMA"){
+        // Para evitar concatenaciones debemos convertir el valor Ingresado a Numero Entero
         result = parseInt(numA) + parseInt(numB);
     }
     else if(Op == 2 || Op.toUpperCase() == "RESTA"){
+        // Para otras operaciones, Javascript infiere que el tipo de dato es Numerico y las realiza automaticamente
         result = numA - numB;
     }
     else if(Op == 3 || Op.toUpperCase() == "PRODUCTO"){
+        // Utilizamos la funcion toUpperCase() debido a que javascript distingue entre Mayusculas y Minusculas
         result = numA * numB;
     }
     else if(Op == 4 || Op.toUpperCase() == "DIVISION"){
+        // En caso que el valor del divisor sea 0 el script No dara error, pero devolvera Infinity
         result = numA / numB;
     }
     else {
+        // En caso que la opcion elegida no corresponde a ninguno de los valores anteriores
         result = "SI NO TENIAS GANAS DE REALIZAR CUENTAS, LO HUBIERAS DICHO DESDE UN PRINCIPIO. ADIOS."
     }
     // Salida de datos
