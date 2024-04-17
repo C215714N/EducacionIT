a22e74fc96dbf4ca064f4934814e4421e3f7d1f1
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-04-17 12:08:14 -0300

    lista de tareas con arrays y eventos

---
+++ scripts/todo.js
@@ -0,0 +1,22 @@
+// Declaraciones
+const d = document;
+const textoTarea = d.querySelector("#tareas input");
+const listaTareas = d.querySelector("#tareas ul");
+const agregarTarea = d.querySelector("#tareas button");
+
+const tareas = [];
+
+// Funciones
+
+// Eventos
+agregarTarea.addEventListener("click", () => {
+    const valor = textoTarea.value;
+    if (valor.length > 3){
+        tareas.push(valor);
+        textoTarea.value = "";
+        listaTareas.innerHTML = "";
+        for(let elemento of tareas){
+            listaTareas.innerHTML+=`<li>${elemento}</li>`
+        }
+    }
+})
