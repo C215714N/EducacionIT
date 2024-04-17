a987d1e198d9a99321601908aed4b0a71ae08cbd
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-04-17 10:29:28 -0300

    implementacion de funciones primitivas y lambda

--- scripts/events.js
+++ scripts/events.js
@@ -1,21 +1,21 @@
 // Declaraciones
 const base = document.getElementById('base');
 const altura = document.getElementById('altura');
+const calculo = document.getElementById('calculo');
 
 // Funciones
-const superficie = (b,h) => b * h 
-const perimetro = (b,h) => b * 2 + h * 2
+const superficie = (b,h) => b * h;
+const perimetro = (b,h) => b * 2 + h * 2;
 
-function result (valor){
-    const resultado = document.getElementById("resultado")
+function result (calculo, base, altura){
+    let valor;
+    const resultado = document.getElementById("resultado");
+    if (calculo == 1) valor = perimetro(base, altura);
+    if (calculo == 2) valor = superficie(base, altura);
     resultado.value = valor;
 }
 
 // Eventos
-base.oninput = function () {
-    result(superficie(base.value, altura.value))
-}
-
-altura.oninput = function () {
-    result(superficie(base.value, altura.value))
-}
+base.oninput = () => result(calculo.value, base.value, altura.value);
+altura.oninput = () => result(calculo.value, base.value, altura.value);
+calculo.oninput = () => result(calculo.value, base.value, altura.value);
