332c20998e42879e0bb3227ff68be93a2c1675a4
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-04-19 10:24:14 -0300

    estilos botones y funciones eliminar y crear lista

--- scripts/todo.js
+++ scripts/todo.js
@@ -7,6 +7,35 @@ const agregarTarea = d.querySelector("#tareas button");
 const tareas = [];
 
 // Funciones
+const crearLista = () => {
+    listaTareas.innerHTML = "";
+    for(let elemento of tareas){
+        listaTareas.innerHTML+=`
+        <li>
+            ${elemento}
+            <button class="btn-edit">Editar</button>
+            <button class="btn-delete">Eliminar</button>
+        </li>`
+    }
+    editarTareas();
+    eliminarTareas();
+}
+const editarTareas = () => {
+    const buttons = d.querySelectorAll("#tareas .btn-edit");
+    console.log(buttons);
+}
+const eliminarTareas = () => {
+    const buttons = d.querySelectorAll("#tareas .btn-delete");
+    // Recorremos el Array de Botones y extraemos cada elemento
+    buttons.forEach( (btn,i) => 
+    // Por cada boton asignamos una accion al evento Click
+    btn.addEventListener( 'click', () => {
+        const seguro = confirm("¿desea eliminar la tarea?");
+        // Eliminamos de Tareas el elemento con el Indice Actual
+        if(seguro) tareas.splice(i,1);
+        crearLista();
+    } ) );
+}
 
 // Eventos
 textoTarea.addEventListener("keyup", (event) => {
@@ -17,15 +46,7 @@ agregarTarea.addEventListener("click", () => {
     if (valor.length > 3){
         tareas.push(valor);
         textoTarea.value = "";
-        listaTareas.innerHTML = "";
-        for(let elemento of tareas){
-            listaTareas.innerHTML+=`
-                <li>
-                    ${elemento}
-                    <button class="btn-edit">Editar</button>
-                    <button class="btn-delete">Eliminar</button>
-                </li>`
-        }
+        crearLista();
         textoTarea.focus();
     }
 })
