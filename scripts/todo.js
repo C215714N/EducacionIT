533d88679623ba1683a64b494d8f77cb7f7fd766
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-04-19 09:41:15 -0300

    agregar botones de accion y aceptar con enter

--- scripts/todo.js
+++ scripts/todo.js
@@ -9,6 +9,9 @@ const tareas = [];
 // Funciones
 
 // Eventos
+textoTarea.addEventListener("keyup", (event) => {
+    if (event.keyCode == 13) agregarTarea.click()
+})
 agregarTarea.addEventListener("click", () => {
     const valor = textoTarea.value;
     if (valor.length > 3){
@@ -16,7 +19,12 @@ agregarTarea.addEventListener("click", () => {
         textoTarea.value = "";
         listaTareas.innerHTML = "";
         for(let elemento of tareas){
-            listaTareas.innerHTML+=`<li>${elemento}</li>`
+            listaTareas.innerHTML+=`
+                <li>
+                    ${elemento}
+                    <button class="btn-edit">Editar</button>
+                    <button class="btn-delete">Eliminar</button>
+                </li>`
         }
         textoTarea.focus();
     }
