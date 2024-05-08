2e3ad52202fa1825355053d88e8c4e4c139ec570
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-05-08 15:15:10 -0300

    estilos y logica de mapa de contacto y pie de pagina

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 // Declaraciones
 const menuBtn = document.getElementById('menu-icon');
 const menuList = document.querySelector('nav .menu');
+const mapBtn = document.querySelector('button.map');
+const mapFrame = document.querySelector('iframe.map');
 // Funciones
 function toggle(element, className = "active"){
-    element.classList.toggle(className);
+    return element.classList.toggle(className);
 }
 // Eventos
 menuBtn.onclick = () => toggle(menuList);
+mapBtn.onclick = () => toggle(mapFrame);
