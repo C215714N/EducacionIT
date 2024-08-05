2b6544124168ff14bdff4fddc48c8f79fecd8101
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-05 16:49:35 -0300

    logica menu hamburguesa

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 /* Declaraciones */
-
+const 
+    d = document,
+    navBtn = d.querySelector('#nav button'),
+    navMenu = d.querySelector('#nav .menu')
+;
 /* Funciones */
-
+const toggle = (element, className = "active") => {
+    return element.classList.toggle(className)
+}
 /* Eventos */
+navBtn.addEventListener('click', () => toggle(navMenu))
