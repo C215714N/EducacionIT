7a8031f7ec48ac375b4f0adfb0e3c68c0f0cdf5a
Cristian Racedo <CristianDRacedo@Hotmail.com>
2026-01-06 15:02:34 -0300

    agregar logica de seleccion de tema

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,15 @@
         tag.classList.add(add);
         tag.classList.remove(drop);
     }
+    const checked = (condition, [a, b])  => condition ? a : b;
+    const bodyCheck = () => document.body.dataset.bsTheme = themeGet();
+    const labelCheck = (condition) => themeLabel.innerText = "Tema " + checked(condition, ['oscuro', 'claro']);
+    const themeGet = () => localStorage.getItem('theme') || 'light';
+    const themeSet = ({target:t}) => {
+        labelCheck(t.checked);
+        localStorage.setItem('theme', checked(t.checked, ['dark', 'light']))
+        bodyCheck();
+    }
     // Formulario de Contacto
     contact.addEventListener('input', (event) => {
         const ctrl = event.target;
@@ -12,4 +21,8 @@
             toggle(ctrl, 'is-valid', 'is-invalid') :
             toggle(ctrl, 'is-invalid', 'is-valid');
     });
+    themeSwitch.addEventListener('input', themeSet)
+
+    bodyCheck()
+    labelCheck(themeGet() === "dark")
 }))()
